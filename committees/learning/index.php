a58e79cbec45d1f78db937efad1792c64e87bf7b
Aditya Vaidyam <avaidyam@purdue.edu>
2016-01-01 20:05:50 -0500

    Update Learning Committee page

--- committees/learning/index.php
+++ committees/learning/index.php
@@ -35,8 +35,10 @@
 			<!-- Content Column -->
 			<div class="col-md-7 text-dark col-md-offset-1">
 				<h2>Learning Committee</h2>
-				<p>The Learning Committee conducts teaching events to expose inexperienced members to a variety of electrical, computer, and other projects.</p>
-				<p>Contact <a href="mailto:lee1509@purdue.edu">John Lee</a> for additional details.</p>
+				<p class="text-center"><img src="/committees/learning/images/learning_header.png" alt="Learning Committee" style="width:90%"></p>
+				<p>The IEEE Learning Committee is dedicated to teaching any member the basics in Software, Hardware, and Mechanical concepts. These concepts vary from soldering, PCB design, JavaScript and Arduino Wiring programming, and 3D CAD modeling for 3D printing. The committee is structured in lessons that students participate in. Each lesson builds towards a final product that encompasses most if not all of the core skills that they will be learning.</p>
+				<p>Contact <a href="mailto:lee1509@purdue.edu">John Lee</a><br>If you’re interested in getting started at any time, let the committee head, John Lee, know via email.</p>
+				<p>Anyone can join at any time!</p>
 			</div>
 		</div> 
 	</div>
