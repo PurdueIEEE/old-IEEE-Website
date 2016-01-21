667a57f0d23cf0c84e1fd81e396e143074313187
Matt Molo <molo.matt@gmail.com>
2016-01-21 12:14:19 -0500

    Everyone forgets to update the copyright notice...

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 	<footer>
 		<div class="row">
 			<div class="col-lg-12">
-				<p>Copyright &copy; Purdue IEEE 2015</p>
+				<p>Copyright &copy; Purdue IEEE <?php echo date("Y") ?></p>
 			</div>
 		</div>
 	</footer>
