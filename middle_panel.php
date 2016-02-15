794e9819ecb20abd92845908bb691053ab92d2b6
Aditya Vaidyam <avaidyam@purdue.edu>
2016-02-15 02:20:11 -0500

    Update sponsors and next event.

--- middle_panel.php
+++ middle_panel.php
@@ -1,2 +1,3 @@
-<h4>Our Spring callout is January 20th!</h4>
-<p>Meet us at 6:30pm in EE 117, and we'll have free Subway!</p>
+<h4>Our first General Assembly is February 16th!</h4>
+<p><b>Industry Night with Intel</b></p>
+<p>Meet us at 5:30pm in RHPH 172, and we'll have free sandwiches!</p>
