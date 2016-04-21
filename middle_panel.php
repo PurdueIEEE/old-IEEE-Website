38377edac6173aede2f4ee4c95df5c084374ee0d
Aditya Vaidyam <avaidyam@users.noreply.github.com>
2016-04-21 17:01:56 -0400

    Update middle_panel.php

--- middle_panel.php
+++ middle_panel.php
@@ -1,2 +1,2 @@
 <h4>Trivia and Committee Updates</h4>
-<p>Meet us Tuesday, March 8th at 6:30pm in ARMS 1010 for committee updates and trivia night!</p>
+<p>Meet us Tuesday, April 26th at 6:30pm in EE 117 for committee updates and trivia night!</p>
