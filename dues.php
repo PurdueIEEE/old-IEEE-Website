fd1e45293bdc1f9beeec1a42b1ed4570a975c823
Brandon Stewart <stewartb2@gmail.com>
2016-08-22 16:52:54 -0400

    Updated dues for 2016 (#26)

--- dues.php
+++ dues.php
@@ -20,7 +20,7 @@
             <div class="col-lg-12 text-dark">
                 <h1 class="text-center">Dues for IEEE</h1>
                 <p>
-                    Purdue IEEE Student Organization follows a simple dues basis for membership. It goes as follows: $5 - One Semester of Membership $8 - 1 Semesters of Membership + IEEE Mug Membership allows each person to have access to IEEE resources, such as:
+                    Purdue IEEE Student Organization follows a simple dues basis for membership. Membership fees are $10 for two semesters, which allows each person to have access to IEEE resources, such as:
                     <ul>
                         <li>Login registration for IEEE computers in EE 014 (IEEE Office)</li>
                         <li>Access to extras at Learning Events by Learning Committee</li>
@@ -28,10 +28,9 @@
                         <li>Free food at General Assemblies</li>
                         <li>Recognition for contributed work with final projects</li>
                     </ul>
-                    To pay dues, please contact the Treasurer, John Lee, at <a href="mailto:IEEE-Treas@purdueieee.org">IEEE-Treas@purdueieee.org</a>, go to EE 014 and use leave your email and dues with a Treasurer-authorized attendant inside the office, or give it to the Treasurer at General Assemblies.
+                    To pay dues, please contact the Treasurer, Bobby Zhang, at <a href="mailto:IEEE-Treas@purdueieee.org">IEEE-Treas@purdueieee.org</a>, go to EE 014 and use leave your email and dues with a Treasurer-authorized attendant inside the office, or give it to the Treasurer at General Assemblies.
                 </p>
 
-                <p class="text-center"><img width="570px" src="/images/ieeedues.jpg"></p> 
 
             </div>
         </div>
