4a84fb08bdd8383bb62dbaca3c476b4e2f2d93fd
Matt Molo <molo.matt@gmail.com>
2016-08-28 17:29:56 -0400

    Missed an amp

--- committees/mtts/index.php
+++ committees/mtts/index.php
@@ -34,7 +34,7 @@
 
             <!-- Content Column -->
             <div class="col-md-7 text-dark col-md-offset-1">
-                <h2>Microwave Theory & Techniques Society</h2>
+                <h2>Microwave Theory &amp; Techniques Society</h2>
                 <p>The mission of Purdue IEEE MTT-S is to explore microwave technology through events that highlight the background and applications of high-frequency technology. The events that Purdue IEEE MTT-S holds cater to the needs of both undergraduate and graduate students. The members of this committee host outreach activities to educate others about the opportunities and benefits of microwaves in communication and radar. The large range of experience levels present give a variety of perspectives in this field and allow for mentorship relations to form for the professional development of all members. Between seminars, roundtable discussions, and demonstrations, the members of MTT-S eagerly support the community that came out of Purdue University’s groundbreaking research in radio frequency and microwave engineering.</p>
                 <p>Send email to <a href="mailto:mtt-s-info@purdueieee.org">mtt-s-info@purdueieee.org</a> for more information.</p>
             </div>
