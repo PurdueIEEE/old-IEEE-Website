80d1db212c5d4328d76c64e7f3eb75f9c89c115b
Matt Molo <molo.matt@gmail.com>
2016-08-30 23:38:08 -0400

    Too many things were active

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                             <h2>IEEE</h2>
                         </div>
                     </div>
-                    <div class="item active">
+                    <div class="item">
                         <div class="fill" style="background-image:url('/images/ieee_callout.jpg');"></div>
                         <div class="carousel-caption">
                             <h2>IEEE Callout: Fall 2016</h2>
