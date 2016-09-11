d8c1d30bdb7352776455bbdfdbc5e1c3aed57183
Matt Molo <molo.matt@gmail.com>
2016-09-11 18:15:23 -0400

    Add racing to calendar, try out the week view

--- calendar.php
+++ calendar.php
@@ -30,15 +30,16 @@
     <div class="well">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <div style="margin: 10px auto; width: 450px; height: 20px; font-weight: 600">
+                <div style="margin: 10px auto; width: 600px; height: 20px; font-weight: 600">
                     <div class="calendar-box" style="background-color: #547AC4;">IEEE Events</div>
                     <div class="calendar-box" style="background-color: #55D4C3;">Computer Society</div>
                     <div class="calendar-box" style="background-color: #2196F3;">EMBS</div>
+                    <div class="calendar-box" style="background-color: #0D7813;">MTT-S</div>
                 </div>
                 <div style="margin: 10px auto; width: 600px; height: 20px; font-weight: 600">
-                    <div class="calendar-box" style="background-color: #0D7813;">MTT-S</div>
                     <div class="calendar-box" style="background-color: #891DDB;">Orbital</div>
                     <div class="calendar-box" style="background-color: #CF5D5D;">PARTIEEE</div>
+                    <div class="calendar-box" style="background-color: #6B3304;">Racing</div>
                     <div class="calendar-box" style="background-color: #E8CB4F;">ROV</div>
                 </div>
                 <?php
@@ -49,6 +50,7 @@
                         array('color' => '0D7813', 'src' => 'edv88gqt4b1v7fs6b6gi2bd8f8@group.calendar.google.com'), #MTT-S
                         array('color' => '5229A3', 'src' => 'thcak7c7b8d7m6up2u5dd13odg@group.calendar.google.com'), #Orbital
                         array('color' => 'A32929', 'src' => 'h8nk778rgq4idlspuf4uevfs84@group.calendar.google.com'), #PARTIEEE
+                        array('color' => '6B3304', 'src' => 'purdueieeeracing%40gmail.com'), #Racing
                         array('color' => 'AB8B00', 'src' => 'ce7l4e2pdhq23tqf3353pfmfp8@group.calendar.google.com'), #ROV 2015-2016
                         array('color' => 'AB8B00', 'src' => '87uv0bmrpiok3ghluf0v4fs5ag@group.calendar.google.com') #ROV 2016-2017
                     );
@@ -61,7 +63,7 @@
                         $list .= $url_encode;
                     }
 
-                    $iframe = "https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;height=750&amp;wkst=1&amp;bgcolor=%23F5F5F5&#10;&amp;".$list."ctz=America%2FNew_York";
+                    $iframe = "https://www.google.com/calendar/embed?mode=WEEK&showTitle=0&amp;showPrint=0&amp;height=750&amp;wkst=1&amp;bgcolor=%23F5F5F5&#10;&amp;".$list."ctz=America%2FNew_York";
                     $link = "https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;height=650&amp;wkst=1&amp;bgcolor=%23ffffff&amp;".$list."ctz=America%2FNew_York";
 
                 ?>
