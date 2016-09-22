57cdfe5336ffa9ce3f430d4c0fb53bf9e23cec5d
Aditya Vaidyam <avaidyam@purdue.edu>
2016-09-22 16:08:31 -0400

    :(

--- header.php
+++ header.php
@@ -15,6 +15,10 @@
     <link href="/assets/bootstrap.css" rel="stylesheet">
     <script src="/assets/jquery.min.js"></script>
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
     <link rel="SHORTCUT ICON" href="/favicon.ico">
     <link rel="apple-touch-icon" href="https://www.ieee.org/ucm/groups/webassets/@ieee/@web/@org/documents/images/ieee_apple_touch_icon.png">
 </head>
