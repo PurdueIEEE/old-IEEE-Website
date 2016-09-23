1d6850702fbdb2169cd660dc7d2b0e99d47bf98b
Matt Molo <molo.matt@gmail.com>
2016-09-23 12:53:48 -0400

    Move learning workshops to past events (#61)

--- learning/index.php
+++ learning/index.php
@@ -12,6 +12,36 @@
                 <h1>IEEE Learning Workshops</h1>
 
                 <h1>Upcoming and In Progess Events:</h1>
+                <h2>CATIA Training Session</h2>
+                <p>This training session will provide a basic overview of CATIA, a CAD software often used in the automotive, aerospace, and other heavy industries. We will delve into the basics of the software, so that members can become familiar with the software and gain a greater understanding of CAD in general. No prior knowledge is necessary. A few take-home projects will be given.</p>
+                <ul>
+                    <li>Lesson 1: Tuesday, September 13th in SC G046 from 7:30 to 9:30 PM</li>
+                    <li>Lesson 2: Sunday, September 18th in SC G046 from 7:30 to 9:30 PM</li>
+                    <li>Lesson 3: Thursday, September 22nd in SC G046 from 7:30 to 9:30 PM</li>
+                </ul>
+                <p><strong>Contact for additional questions and help:</strong></p>
+                <ul>
+                    <li>Yash Nain: ynain@purdue.edu</li>
+                    <li>Michael Malachowski: mmalacho@purdue.edu</li>
+                    <li>Joey Williamson: will1361@purdue.edu</li>
+                    <li>Robert McIntyre: mcintyr2@purdue.edu</li>
+                    <li>Ajay Kumar: ajayk@purdue.edu</li>
+                </ul>
+                <p>Yash will remain for an extra hour to answer any questions, and will hold officer hours every Friday from 10:00 A.M. to 1:00 P.M. He will not be available for the last session.</p>
+
+                <h2>Electric Vehicle Technology</h2>
+                <p>This seminar will be focused on the various aspects of electric vehicle technology. Throughout the session, we will go over what goes in to modern day electric vehicles and how they relate to student projects. There will be an emphasis on the evGrandPrix Go-Karts that are built each year to race in the electric counterpart to the GrandPrix at the Indianapolis Motor Speedway.</p>
+                <ul>
+                    <li>Monday, September 26th in ME 1051 from 6:30 to 7:30 PM</li>
+                </ul>
+                <p><strong>Contact for additional questions and help:</strong></p>
+                <ul>
+                    <li>Steven Padilla: spadilla@purdue.edu</li>
+                </ul>
+                <p>Steven will be available for office hours for additional help Thursday 22, from 2:30 to 3:20 PM in EE 014</p>
+
+                <h1>Past Events:</h1>
+
                 <h2>EAGLE Training Session</h2>
                 <p>This training session will cover the basics of EAGLE, an electrical board schematic and layout tool. The training session is split into three parts, that will teach how to create schematics, board layouts, and parts. No prior knowledge is required. The first session will be held this Thursday, in Physics 203 at 6:30 PM. Additional details below:</p>
                 <ul>
@@ -49,7 +79,6 @@
                     <li>Aditya Vaidyam: avaidyam@purdue.edu</li>
                 </ul>
 
-
                 <h2>SolidWorks Training Session</h2>
                 <p>Learn the basics of 3D modeling with one of the most widely utilized software’s in industry, SolidWorks! This introductory series will cover basic parametric modeling with a focus on best practices so that you can create better, more robust models. The training session is split into three parts, with the first session being held this Wednesday, in Physics 022 at 6:30 PM. Additional details below:</p>
                 <ul>
@@ -70,37 +99,6 @@
                     <li>Sophia Duan: duan35@purdue.edu (12:30 - 1:20 PM TR) EE 014</li>
                     <li>Joe Pejril: jpejril@purdue.edu (10:30 - 11:20 AM TR) EE 014</li>
                 </ul>
-
-
-                <h2>CATIA Training Session</h2>
-                <p>This training session will provide a basic overview of CATIA, a CAD software often used in the automotive, aerospace, and other heavy industries. We will delve into the basics of the software, so that members can become familiar with the software and gain a greater understanding of CAD in general. No prior knowledge is necessary. A few take-home projects will be given.</p>
-                <ul>
-                    <li>Lesson 1: Tuesday, September 13th in SC G046 from 7:30 to 9:30 PM</li>
-                    <li>Lesson 2: Sunday, September 18th in SC G046 from 7:30 to 9:30 PM</li>
-                    <li>Lesson 3: Thursday, September 22nd in SC G046 from 7:30 to 9:30 PM</li>
-                </ul>
-                <p><strong>Contact for additional questions and help:</strong></p>
-                <ul>
-                    <li>Yash Nain: ynain@purdue.edu</li>
-                    <li>Michael Malachowski: mmalacho@purdue.edu</li>
-                    <li>Joey Williamson: will1361@purdue.edu</li>
-                    <li>Robert McIntyre: mcintyr2@purdue.edu</li>
-                    <li>Ajay Kumar: ajayk@purdue.edu</li>
-                </ul>
-                <p>Yash will remain for an extra hour to answer any questions, and will hold officer hours every Friday from 10:00 A.M. to 1:00 P.M. He will not be available for the last session.</p>
-
-                <h2>Electric Vehicle Technology</h2>
-                <p>This seminar will be focused on the various aspects of electric vehicle technology. Throughout the session, we will go over what goes in to modern day electric vehicles and how they relate to student projects. There will be an emphasis on the evGrandPrix Go-Karts that are built each year to race in the electric counterpart to the GrandPrix at the Indianapolis Motor Speedway.</p>
-                <ul>
-                    <li>Monday, September 26th in ME 1051 from 6:30 to 7:30 PM</li>
-                </ul>
-                <p><strong>Contact for additional questions and help:</strong></p>
-                <ul>
-                    <li>Steven Padilla: spadilla@purdue.edu</li>
-                </ul>
-                <p>Steven will be available for office hours for additional help Thursday 22, from 2:30 to 3:20 PM in EE 014</p>
-
-                <h1>Past Events:</h1>
 
                 <h2>Git + GitHub</h2>
                 <p>This session will cover the basics of using git and GitHub. Git is a version control software that makes saving your old code and reverting changes much easier. GitHub is a remote location to store your code and collaborate with other people through merging and code reviews. The session will cover only command line tools, so familiarity with the command line is helpful.</p>
