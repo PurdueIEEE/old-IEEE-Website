211c7f4270e9dbf1b03bec8c21c5b95133d21943
mattmolo <molo.matt@gmail.com>
2016-09-23 12:36:53 -0400

    Remove join into until brandon gets the content

--- join.php
+++ join.php
@@ -8,26 +8,6 @@
 
     <div class="well">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2 text-dark">
-                <h1 class="text-center">Joining IEEE</h1>
-                <p>
-                    Purdue IEEE Student Organization follows a simple dues basis for membership. Membership fees are $10 for two semesters, which allows each person to have access to IEEE resources, such as:
-                    <ul>
-                        <li>Access to extras at Learning Events by Learning Committee</li>
-                        <li>Trip expense coverage for committee competitions and social events</li>
-                        <li>Free food at General Assemblies</li>
-                        <li>Recognition for contributed work with final projects</li>
-                    </ul>
-                    To pay dues, please contact the Treasurer, Bobby Zhang, at <a href="mailto:IEEE-Treas@purdueieee.org">IEEE-Treas@purdueieee.org</a>, go to EE 014 and use leave your email and dues with a Treasurer-authorized attendant inside the office, or give it to the Treasurer at General Assemblies.
-                </p>
-
-
-            </div>
-        </div>
-    </div>
-
-    <div class="well">
-        <div class="row">
             <div class="col-lg-12 text-dark">
                 <h1 class="text-center">Subscribe to our Mailing Lists</h1>
 
