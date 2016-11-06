5d50c0e8c737d9f79d5ab63899c7bfe8eb5012db
Kyle Rakos <krakos@purdue.edu>
2016-11-06 14:11:53 -0500

    Fixed more merge conflicts

--- rov/sponsors/2015.php
+++ rov/sponsors/2015.php
@@ -23,8 +23,7 @@
         <div class="col-md-7 text-dark col-md-offset-1">
         <h1>Sponsors</h1>
             <p>Without the help of our generous sponsors, the ROV team would be unable to compete. We are very thankful for their support. </p>
-            <p>If you are interested in supporting the team at various levels please contact <a href="mailto:krakos@purdue.edu">Kyle Rakos</a>.
-            Also feel free to check out our <a href="2015.php">2015 - 2016 sponsors page</a></p><br>
+            <p>If you are interested in supporting the team at various levels please contact <a href="mailto:krakos@purdue.edu">Kyle Rakos</a>.</p><br>
 
             <h2 class="text-center">Platinum Partner</h2>
             <div class="row">
@@ -32,17 +31,10 @@
                     <a href="http://engineering.purdue.edu/INSGC" target="_blank"><img src="/rov/images/INSGC.png" alt="INSGC" style="width:100%"></a>
                 </div>
                 <div class="col-md-4 text-center">
-<<<<<<< HEAD:committees/rov/sponsors/index.php
-                    <a href="http://www.solidworks.com/" target="_blank"><img src="/committees/rov/images/solidworks.png" alt="SolidWorks" style="width:100%"></a>
-                </div>
-                <div class="col-md-4 text-center">
-                    <a href="http://boeing.com" target="_blank"><img src="/committees/rov/images/Boeing.png" alt="Boeing" style="width:100%"></a>
-=======
                     <a href="http://www.purdue.edu/provost/" target="_blank"><img src="/rov/images/Provost.png" alt="Purdue Office of the Provost" style="width:100%"></a>
                 </div>
                 <div class="col-md-4 text-center">
                     <a href="http://purdueesc.org/" target="_blank"><img src="/rov/images/PESC.png" alt="Purdue Engineering Student Council" style="width:100%"></a>
->>>>>>> upstream/master:rov/sponsors/2015.php
                 </div>
             </div>
 
@@ -50,14 +42,6 @@
             <h2 class="text-center">Gold Partner</h2>
             <div class="row">
                 <div class="col-md-4 text-center">
-<<<<<<< HEAD:committees/rov/sponsors/index.php
-                    
-                </div>
-                <div class="col-md-4 text-center">
-                    
-                </div>
-                <div class="col-md-4 text-center">
-=======
                     <a href="https://engineering.purdue.edu/Engr" target="_blank"><img src="/rov/images/Engineering.png" alt="Purdue College of Engineering" style="width:100%"></a>
                 </div>
                 <div class="col-md-4 text-center">
@@ -65,7 +49,6 @@
                 </div>
                 <div class="col-md-4 text-center">
                     <a href="http://www.cadsoftusa.com/" target="_blank"><img src="/rov/images/eagle.png" alt="Cadsoft" style="width:100%"></a>
->>>>>>> upstream/master:rov/sponsors/2015.php
                 </div>
             </div>
 
@@ -76,10 +59,7 @@
 
                 </div>
                 <div class="col-md-4 text-center">
-<<<<<<< HEAD:committees/rov/sponsors/index.php
-=======
                     <a href="http://www.rssgrinders.com/" target="_blank"><img src="/rov/images/rss.png" alt="RSS Grinders and Automation" style="width:100%"></a>
->>>>>>> upstream/master:rov/sponsors/2015.php
                 </div>
                 <div class="col-md-4 text-center">
 
@@ -90,12 +70,6 @@
             <h2 class="text-center">Bronze Partner</h2>
             <div class="row">
                 <div class="col-md-4 text-center">
-<<<<<<< HEAD:committees/rov/sponsors/index.php
-                    </div>
-                    <div class="col-md-4 text-center">
-                    </div>
-                    <div class="col-md-4 text-center">
-=======
                     <a href="http://www.bechtel.com/" target="_blank"><img src="/rov/images/Bechtel.png" alt="Bechtel" style="width:100%">
                     </div>
                     <div class="col-md-4 text-center">
@@ -103,20 +77,11 @@
                     </div>
                     <div class="col-md-4 text-center">
                         <a href="http://www.eastman.com/Pages/Home.aspx" target="_blank"><img src="/rov/images/eastman.png" alt="Eastman Chemical" style="width:100%"></a>
->>>>>>> upstream/master:rov/sponsors/2015.php
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col-md-4 text-center">
-<<<<<<< HEAD:committees/rov/sponsors/index.php
-                        
-                    </div>
-                    <div class="col-md-4 text-center">
-                        <a href="http://www.digikey.com/" target="_blank"><img src="/committees/rov/images/DigiKey.png" alt="DigiKey" style="width:100%"></a>
-                    </div>
-                    <div class="col-md-4 text-center">
-=======
                         <a href="http://www.digikey.com/" target="_blank"><img src="/rov/images/DigiKey.png" alt="DigiKey" style="width:100%"></a>
                     </div>
                     <div class="col-md-4 text-center">
@@ -124,18 +89,11 @@
                     </div>
                     <div class="col-md-4 text-center">
                         <a href="http://www.northropgrumman.com/Pages/default.aspx" target="_blank"><img src="/rov/images/northrop.png" alt="Northrop Grumman" style="width:100%"></a>
->>>>>>> upstream/master:rov/sponsors/2015.php
                     </div>
                 </div>
 
                 <div class="row" style="margin-top: 20px">
                     <div class="col-md-4 text-center">
-<<<<<<< HEAD:committees/rov/sponsors/index.php
-                    </div>
-                    <div class="col-md-4 text-center">
-                    </div>
-                    <div class="col-md-4 text-center">
-=======
                         <a href="http://wabashpower.com" target="_blank"><img src="/rov/images/WabashPower.jpg" alt="Wabash Power Equipment" style="width:100%"></a>
                     </div>
                     <div class="col-md-4 text-center">
@@ -143,7 +101,6 @@
                     </div>
                     <div class="col-md-4 text-center">
                         <a href="http://www.netgear.com/" target="_blank"><img src="/rov/images/Netgear.png" alt="Netgear" style="width:100%"></a>
->>>>>>> upstream/master:rov/sponsors/2015.php
                     </div>
                 </div>
             </div>
