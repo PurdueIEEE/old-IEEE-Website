9f34d89eeb13cd37bbe3c3d9dc8e6b71718c8b0c
Kyle <rakoskr6@users.noreply.github.com>
2016-11-10 15:48:36 -0500

    Added new ROV pictures (#71)
    * Added new ROV pictures
    
    * Got rid of some spaces, tabs, and added Henry
    
    * Indentation fixes, whitespace issues, bad formatting...

--- rov/index.php
+++ rov/index.php
@@ -23,64 +23,134 @@
         <div class="col-md-7 text-dark col-md-offset-1">
 
             <h2 class="text-center">2016 - 2017 Season</h2>
-            <p> The ROV Team began its ninth season with over 60 new-member applicants. The top 29 applicants were accepted to join the 30 returning veterans in what will be the the
-                strongest team to date! Our team looks forward to the summer 2017 competition in Long Beach, CA and are eagerly preparing for first place!</p>
+            <p>
+                The ROV Team began its ninth season with over 60 new-member applicants. The top 29 applicants were accepted to join the 30 returning veterans in what will be the the strongest team to date! Our team looks forward to the summer 2017 competition in Long Beach, CA and are eagerly preparing for first place!
+            </p>
+            <br>
 
-                
-                <br>
-
-                <h2 class="text-center">About The ROV Team</h2>
-
-                <p class="text-justify">The Remotely Operated underwater Vehicle (ROV) team was founded as a committee within Purdue IEEE Student
-                    Organization in 2008 with a small but dedicated group of five students. Today, the team's mission is to foster
-                    technical and professional skills of its members by designing, constructing, and testing an innovative
-                    underwater vehicle to compete in the MATE Center International ROV Competition.
-                </p>
-
-                <p class="text-justify">The ROV team is comprised of both engineering and non-engineering students working collaboratively on a
-                    hands-on project from the vehicle's design and prototyping phase, to its construction and testing, and ultimately
-                    competition performance. The team fosters a multidisciplinary spirit so that the
-                    best ideas may come forth. In addition to the mission, the MATE competition also requires a professional
-                    presentation, public display poster, and technical documentation.
-                </p>
-
-                <p class="text-justify">Because the vehicle requires excellence in
-                    design, construction, and presentation, the team members are themselves multidisciplinary collaborators and
-                    experts in their own discipline. Members learn to communicate clearly
-                    with each other and with clients while managing their own project responsibilities. Through various outreach events
-                    during the year, the team aims to inspire future generations to enter STEM (Science, Technology, Engineering,
-                    and Mathematics) fields.
-                </p>
-
-                <p class="text-justify">For more current and past information view the links to the left or
-                    contact <a href="mailto:krakos@purdue.edu">Kyle Rakos</a>.
-                </p>
-                <p class="text-center"><a href="https://www.facebook.com/purdueieeerov"><i class="fa fa-facebook-square fa-4x"></i></a></p>
-                <br>
-
-                <h2 class="text-center">2015 - 2016 Pictures</h2>
-                <p class="text-center">(See <a href="history/">history</a> for more pictures)</p>
-                <br>
-
-
-                <h4 class="text-center">Team at the International Competition in front of NASA's Orion Test Capsule</h4>
-                <p class="text-center"> <img src="/rov/images/TeamOrion.JPG" alt="2015 - 2016 Team" style="width:100%"></p><br>
-                <br>
-                <br>
-
-                <h4 class="text-center">ROV Maelstrom</h4>
-                <p class="text-center"> <img src="/rov/images/ROVMaelstrom.jpg" alt="Maelstrom" style="width:100%"></p><br>
-                
-
-                <h4 class="text-center">ROV Maelstrom Rendering</h4>
-                <p class="text-center"> <img src="/rov/images/X8.JPG" alt="Maelstrom Rendering" style="width:100%"></p><br>
-
-
-
+            <h2 class="text-center">About The ROV Team</h2>
+            <div class="col-md-6">
+                <img src="/rov/images/ROVMaelstrom.jpg" alt="Maelstrom" style="width:100%" style="float:left" style="display:block">
+                <p class="text-center">2015-2016 ROV <em>Maelstrom</em></p>
             </div>
 
-        </div>
+            <div class="col-md-6">
+                <p class="text-justify" style="float:left">
+                    The Remotely Operated underwater Vehicle (ROV) team was founded as a committee within Purdue IEEE Student Organization in 2008 with a small but dedicated group of five students. Today, the team's mission is to foster technical and professional skills of its members by designing, constructing, and testing an innovative underwater vehicle to compete in the MATE Center International ROV Competition.
+                </p>
+            </div>
 
+            <p class="text-justify">
+                The ROV team is comprised of both engineering and non-engineering students working collaboratively on a hands-on project from the vehicle's design and prototyping phase, to its construction and testing, and ultimately competition performance. The team fosters a multidisciplinary spirit so that the best ideas may come forth. In addition to the mission, the MATE competition also requires a professional presentation, public display poster, and technical documentation.
+            </p>
+
+            <p class="text-justify">
+                Because the vehicle requires excellence in design, construction, and presentation, the team members are themselves multidisciplinary collaborators and experts in their own discipline. Members learn to communicate clearly with each other and with clients while managing their own project responsibilities. Through various outreach events during the year, the team aims to inspire future generations to enter STEM (Science, Technology, Engineering, and Mathematics) fields.
+            </p>
+
+            <p class="text-justify">
+                For more current and past information view the links to the left or contact <a href="mailto:krakos@purdue.edu">Kyle Rakos</a>. The ROV team is currently not accepting new members (a new application will open next semester if needed). The current team members are below.
+            </p>
+
+
+            <h2 class="text-center">2016-2017 Team Pictures</h2>
+            <h3 class="text-center">Full Team Picture</h3>
+            <p class="text-center"><img src="/rov/images/team_fall_2016.jpg" alt="Full Team" style="width:100%"></p><br>
+
+            <h3 class="text-center">Electrical Team</h3>
+            <p class="text-center"><img src="/rov/images/electrical_team_fall_2016.jpg" alt="Electrical Team" style="width:100%"></p><br>
+
+
+            <div class="row">
+                <div class="col-sm-2"><p class="text-center">Rodolfo<br>Leiva</p></div>
+                <div class="col-sm-2"><p class="text-center">Samuel<br>Deghuee</p></div>
+                <div class="col-sm-2"><p class="text-center">Reshef<br>Elisha</p></div>
+                <div class="col-sm-2"><p class="text-center">Luke<br>McBee</p></div>
+                <div class="col-sm-2"><p class="text-center">Jason<br>King</p></div>
+                <div class="col-sm-2"><p class="text-center">James<br>Scholl</p></div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-sm-2"><p class="text-center">Ashay<br>Devanur</p></div>
+                <div class="col-sm-2"><p class="text-center">Jessica<br>Chin</p></div>
+                <div class="col-sm-2"><p class="text-center">Carolyn<br>Lewelling</p></div>
+                <div class="col-sm-2"><p class="text-center">Sreyas<br>Mirthipati</p></div>
+                <div class="col-sm-2"><p class="text-center">Alan<br>Han</p></div>
+                <div class="col-sm-2"><p class="text-center">Xi<br>Wu</p></div>
+            </div>
+
+
+            <h3 class="text-center">Mechanical Team</h3>
+            <p class="text-center"><img src="/rov/images/mechanical_team_fall_2016.jpg" alt="Mechanical Team" style="width:100%"></p><br>
+            <div class="row">
+                <div class="col-sm-2"><p class="text-center">Amandeep<br>Singh</p></div>
+                <div class="col-sm-1"><p class="text-center">Katie<br>Mao</p></div>
+                <div class="col-sm-1"><p class="text-center">Katie<br>Lothrop</p></div>
+                <div class="col-sm-2"><p class="text-center">Joe<br>Pejril</p></div>
+                <div class="col-sm-1"><p class="text-center">Joshua<br>Berg</p></div>
+                <div class="col-sm-2"><p class="text-center">Alex<br>Ruffino</p></div>
+                <div class="col-sm-2"><p class="text-center">Tyler<br>Stagge</p></div>
+                <div class="col-sm-1"><p class="text-center">Jonathan<br>Kline</p></div>
+            </div>
+            <br>
+
+            <div class="row">
+                <div class="col-sm-2"><p class="text-center">Jiajun<br>An</p></div>
+                <div class="col-sm-1"><p class="text-center"><br></p></div>
+                <div class="col-sm-1"><p class="text-center">Monica<br>Mah</p></div>
+                <div class="col-sm-2"><p class="text-center">Bayley<br>Clausen</p></div>
+                <div class="col-sm-1"><p class="text-center">Sanay<br>Shah</p></div>
+                <div class="col-sm-2"><p class="text-center">Alex<br>Gebhardt</p></div>
+                <div class="col-sm-2"><p class="text-center">Ben<br>Hutchins</p></div>
+                <div class="col-sm-1"><p class="text-center">Seth<br>Emond</p></div>
+            </div>
+            <div>
+                <p>Not Pictured: Teal Dowd</p>
+            </div>
+
+
+            <h3 class="text-center">Software Team</h3>
+            <p class="text-center"><img src="/rov/images/software_team_fall_2016.jpg" alt="Software Team" style="width:100%"></p><br>
+            <div class="row">
+                <div class="col-sm-1"><p class="text-center">Joey<br>Danial</p></div>
+                <div class="col-sm-2"><p class="text-center">Zidaan<br>Dutta</p></div>
+                <div class="col-sm-1"><p class="text-center">Andrew<br>Bostock</p></div>
+                <div class="col-sm-2"><p class="text-center">Matt<br>Molo</p></div>
+                <div class="col-sm-1"><p class="text-center">Brandon<br>Stewart</p></div>
+                <div class="col-sm-1"><p class="text-center">Ben<br>Maxfield</p></div>
+                <div class="col-sm-2"><p class="text-center">Ryan<br>McBee</p></div>
+                <div class="col-sm-1"><p class="text-center">Sam<br>Jenkins</p></div>
+                <div class="col-sm-1"><p class="text-center">Ian<br>Sibley</p></div>
+            </div>
+            <br>
+
+            <div class="row">
+                <div class="col-sm-1"><p class="text-center">Joel<br>Copi</p></div>
+                <div class="col-sm-2"><p class="text-center">Nathan<br>Glotzbach</p></div>
+                <div class="col-sm-1"><p class="text-center">Charles<br>Li</p></div>
+                <div class="col-sm-2"><p class="text-center">Mimansa<br>Verma</p></div>
+                <div class="col-sm-1"><p class="text-center">Yuqin<br>Duan</p></div>
+                <div class="col-sm-1"><p class="text-center">Ajay<br>Gopakumar</p></div>
+                <div class="col-sm-2"><p class="text-center">Samuale<br>Yigrem</p></div>
+                <div class="col-sm-1"><p class="text-center">Kendal<br>Norman</p></div>
+                <div class="col-sm-1"><p class="text-center"><br></p></div>
+            </div>
+
+            <h3 class="text-center">Administrative Team</h3>
+            <p class="text-center"><img src="/rov/images/administrative_team_fall_2016.jpg" alt="Administrative Team" style="width:100%"></p><br>
+            <div class="row">
+                <div class="col-sm-2"><p class="text-center">Matt Molo<br><em>Software Lead</em></p></div>
+                <div class="col-sm-2"><p class="text-center">Joshua Berg<br><em>Mechanical Lead</em></p></div>
+                <div class="col-sm-2"><p class="text-center">Carolyn Lewelling<br><em>Sponsorship Coordinator</em></p></div>
+                <div class="col-sm-2"><p class="text-center">Alex Ruffino<br><em>Vice Captain</em></p></div>
+                <div class="col-sm-2"><p class="text-center">Kyle Rakos<br><em>Captain</em></p></div>
+                <div class="col-sm-2"><p class="text-center">Luke McBee<br><em>Electrical Lead</em></p></div>
+            </div>
+            <p>Not Pictured: Henry Shi <em>(Technical Writter)</em></p>
+
+            <p class="text-center"><a href="https://www.facebook.com/purdueieeerov"><i class="fa fa-facebook-square fa-4x"></i></a></p>
+            <br>
+        </div>
     </div>
 </div>
 <!-- /.well -->
