53aaf479dcf37b33629052cf1bf9a121227abdfe
mikitymike <mikitymike@gmail.com>
2016-11-13 01:36:18 -0500

    finish up junk

--- partieee/index.php
+++ partieee/index.php
@@ -11,12 +11,12 @@
 
     <div class="fullscreen-bg">
     <video loop muted autoplay poster="images/Genie.jpg" id="bg-vid" class="fullscreen-bg__video">
-        <source src="videos/forest.mov" type="video/webm">
+        <source src="videos/backgroundl" type="video/webm">
     	</video>
     </div>
     <script type="text/javascript">
       /* set playback rate for bg video */
-      document.getElementById("bg-vid").playbackRate = .75;
+      document.getElementById("bg-vid").playbackRate = .7;
        
     </script>
         <div class="row well nopad">
@@ -55,15 +55,39 @@
             </header>
         </div>
     </div>
-        <div class="well card-1 text-dark">
+    <div class="text-dark">
+        <div class="row text-center">
+            <div class="col-lg-12">
+                <ul class="pagination">
+                    <li>
+                        <a href="http://www.auvsi-suas.org/competitions/2017/">SUAS Competition Info</a>
+                    </li>
+                    <li>
+                        <a href="/professional/">Info for Sponsors</a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/channel/UCDyoVLkPq5463PZMnQ-G2hA">Youtube</a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/purdueaerial/">Facebook</a>
+                    </li>
+                    <li>
+                        <a href="https://drive.google.com/folderview?id=0B5107H94KnS1Q0dDOEJ5TTU0OEk&usp=sharing">Pictures</a>
+                    </li>
+                </ul>
+            </div>
+	</div>
+    </div>
+       <div class="well card-1 text-light">
+	 
             <div class="row text-center">
                 <h1 id="mission">Mission</h1>
                 <hr />
-                <p>Purdue Aerial Robotics Team IEEE (PARTIEEE), founded in 1996, consists of four teams: AeroMechanical, Electrical, Software, and Writing. The AeroMechanical team designs and manufactures a custom, stable fixed wing aircraft based on mathematical models. The Electrical team develops custom hardware for the plane, such as power supply boards, sensor boards, and flight controllers. The Software team writes autopilot and image processing algorithms that will guide the system through waypoints and targets. The Writing team works on the technical journal paper, documentation, and presentation displays. The contributions from each team are blended into an unmanned aerial system (UAS) and evaluated at the AUVSI SUAS Competition at Webster Field, Maryland during the summer. <a href="https://drive.google.com/folderview?id=0B5107H94KnS1Q0dDOEJ5TTU0OEk&usp=sharing">Here is a link</a> to media relevant to the team's progress over the past year. For more information contact <a href="mailto:partieee@purdue.edu">Michael Malachowski</a></p>
+                <p>Purdue Aerial Robotics Team IEEE (PARTIEEE), founded in 1996, consists of four teams: AeroMechanical, Electrical, Software, and Writing. The AeroMechanical team designs and manufactures a custom, stable fixed wing aircraft based on mathematical models. The Electrical team develops custom hardware for the plane, such as power supply boards, sensor boards, and flight controllers. The Software team writes autopilot and image processing algorithms that will guide the system through waypoints and targets. The Writing team works on the technical journal paper, documentation, and presentation displays. The contributions from each team are blended into an unmanned aerial system (UAS) and evaluated at the AUVSI SUAS Competition at Webster Field, Maryland during the summer. For more information contact <a href="mailto:partieee@purdue.edu">Michael Malachowski</a></p>
                 <br />
             </div>
         </div>
-        <div class="well card-1 text-dark">
+        <div class="well card-1 text-light">
             <div class="row text-center">
                 <h1 id="officers">Officers</h1>
                 <hr />
@@ -76,121 +100,64 @@
                     </div>
                     <!--Jon Paul Pepen-->
                     <div class= "col-md-4">
-                        <img src="./images/jon-paul.jpg" alt= "Jon Paul Pepen" class="img-circle" width=250px height=250px />
+                        <img src="./images/jon-paul-pepen.jpg" alt= "Jon Paul Pepen" class="img-circle" width=250px height=250px />
                         <h4>Jon Paul Pepen</h4>
-                        <p>First Officer<br />Major<br />Senior</p>
+                        <p>First Officer<br />Aeronautics and Astronautics Engineering<br />Senior</p>
                     </div>
                     <!--Ajay-->
                     <div class= "col-md-4">
-                        <img src="./images/ajay.png" alt= "Ajay Kumar" class="img-circle" width=250px height=250px />
+                        <img src="./images/ajay-kumar.jpg" alt= "Ajay Kumar" class="img-circle" width=250px height=250px />
                         <h4>Ajay Kumar</h4>
-                        <p>Secretary<br />Major<br />Senior</p>
+                        <p>Secretary<br />Mechanical Engineering<br />Senior</p>
                     </div>
                 </div>
                 <br />
                 <div class="row">
                     <!--Joey-->
                     <div class= "col-md-4">
-                        <img src="./images/joey.jpg" alt= "Joey Williamson" class="img-circle" width=250px height=250px />
+                        <img src="./images/joey-williamson.jpg" alt= "Joey Williamson" class="img-circle" width=250px height=250px />
                         <h4>Joey Williamson</h4>
-                        <p>Aeromech Team Lead<br />Major<br />Year</p>
+                        <p>Aeromech Team Lead<br />Aeronautics and Astronautics Engineering<br />Sophomore</p>
                     </div>
                     <!--Patrick-->
                     <div class= "col-md-4">
-                        <img src="./images/patrick.jpg" alt= "Patrick May" class="img-circle" width=250px height=250px />
+                        <img src="./images/patrick-may.png" alt= "Patrick May" class="img-circle" width=250px height=250px />
                         <h4>Patrick May</h4>
-                        <p>Electrical Team Lead<br />Electrical Engineering<br />Year</p>
+                        <p>Electrical Team Lead<br />Electrical Engineering<br />Junior</p>
                     </div>
                     <!--Ellie-->
                     <div class= "col-md-4">
-                        <img src="./images/ellie.jpg" alt= "Ellie Topi" class="img-circle" width=250px height=250px />
+                        <img src="./images/ellie-topi.jpg" alt= "Ellie Topi" class="img-circle" width=250px height=250px />
                         <h4>Ellie Topi</h4>
-                        <p>Lab Manager<br />Major<br />Year</p>
+                        <p>Lab Manager<br />Mechanical Engineering Technology<br />Junior</p>
                     </div>
                 </div>
                     <br />
                 <div class="row">
-                    <!--George-->
-                    <div class= "col-md-4">
-                        <img src="./images/george.jpg" alt= "George Xu" class="img-circle" width=250px height=250px />
-                        <h4>George Xu</h4>
-                        <p>Aeromech Technical Lead<br />Major<br />Year</p>
-                    </div>
                     <!--Robert-->
                     <div class= "col-md-4">
-                        <img src="./images/robert.jpg" alt= "Robert McIntyre" class="img-circle" width=250px height=250px />
+                        <img src="./images/robert-mcintyre.jpg" alt= "Robert McIntyre" class="img-circle" width=250px height=250px />
                         <h4>Robert McIntyre</h4>
-                        <p>Aeromech Manufacturing Lead<br />Major<br />Year</p>
+                        <p>Aeromech Manufacturing Lead<br />Mechanical Engineering<br />Sophomore</p>
                     </div>
                     <!--Yash-->
                     <div class= "col-md-4">
                         <img src="../images/officers/Yash_Nain.jpg" alt= "Yash Nain" class="img-circle" width=250px height=250px />
                         <h4>Yash Nain</h4>
-                        <p>Sponsorship and Relations Coordinator<br />Major<br />Year</p>
+                        <p>Sponsorship and Relations Coordinator<br />Multidisciplinary Engineering<br />Junior</p>
                     </div>
                 </div>
                 
             </div>
         </div>
-        <div class="well card-1 text-dark">
+        <div class="well card-1 text-light">
             <div class="row text-center">
-                <h1 id="events">Past Events</h1>
+                <h1 id="events">Project Updates</h1>
                 <hr />
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="./images/Sabbagh.gif" alt= "Placeholder" class="img-rounded" width=200px height=250px />
-                        <h4>Dr. Harold Sabbagh</h4>
-                        <p>Modeling neuronal functionality in PSPICE as an electrical circuit.</p>
-                    </div>
-                    <div class="col-md-6">
-                        <img src="./images/rocky.jpg" alt= "Placeholder" class="img-rounded" width=200px height=250px />
-                        <h4>Dr. Rocky Clancy</h4>
-                        <p>Engineering Ethics.</p>
-                    </div>
-                </div>
-                <br /><br />
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="./images/amy.jpg" alt= "Placeholder" class="img-rounded" width=220px height=250px />
-                        <h4>Dr. Amy Brewster</h4>
-                        <p>Neuroethics.</p>
-                    </div>
-                    <div class="col-md-6">
-                        <img src="./images/song.jpg" alt= "Placeholder" class="img-rounded" width=200px height=250px />
-                        <h4>Dr. Yujia Song</h4>
-                        <p>Biomedical Ethics.</p>
-                    </div>
-                </div>
+               
                 <br /><br />
             </div>
         </div>
-        <div class="well card-1 text-dark">
-            <div class="row text-center">
-                <h1 id="projects">Projects</h1>
-                <hr />
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="./images/EEG.jpg" alt= "EEG" class="img-rounded" width=300px height=250px />
-                    </div>
-                    <div class="col-md-6">
-                        <br /><br />
-                        <h4>Electroencephalogram (EEG)</h4>
-                        <p>An EEG can be used to interpret the brain's neural activity at a low resultion for both clinical diagnostics and to develop assistive devices.</p>
-                    </div>
-                </div>
-                <br /><br />
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="./images/CoopWithDog.jpg" alt= "Zeus?" class="img-rounded" width=300px height=250px />
-                    </div>
-                    <div class="col-md-6">
-                        <br /><br />
-                        <h4>Dog Shelter</h4>
-                        <p>We're partnering with Circle-K on volunteering in the local community, building biomedical instruments and devices to aid veterinarians and dogs alike.</p>
-                    </div>
-                </div>
-            </div>
-    </div>
-    <!-- /.well -->
+
 
 <?php include '../footer.php'; ?>
