283b8476c8ed4b4a87d97a6b903dc83d28fc8439
Kyle <rakoskr6@users.noreply.github.com>
2016-12-08 14:27:21 -0500

    Added a join ROV page and updated main ROV page (#73)

---
+++ rov/join.php
@@ -0,0 +1,46 @@
+<?php
+    $page_title = 'Home';
+    $site_title = 'IEEE ROV';
+    $site_index = '/rov';
+    include '../header.php';
+?>
+
+<!-- Content Row -->
+<div class="well card-1">
+    <div class="row">
+
+        <!-- Sidebar Column -->
+        <div class="col-md-3">
+            <div class="list-group">
+                <a href="/rov/history/" class="list-group-item">History</a>
+                <a href="/rov/bylaws/" class="list-group-item">Bylaws</a>
+                <a href="/rov/sponsors/" class="list-group-item">Sponsors</a>
+                <a href="http://rovwiki.pieee.org/" class="list-group-item">Wiki<i class="material-icons" style="font-size: 16px; margin-left: 5px;">exit_to_app</i></a>
+            </div>
+        </div>
+
+        <!-- Content Column -->
+        <div class="col-md-7 text-dark col-md-offset-1">
+
+            <h2 class="text-center">Join ROV</h2>
+            <p>
+                The ROV team brings together expertise in electronics, mechanics, and software in order to create an unrivled vehicle. We intend to finish first place in the world, and we are therefore looking for dedicated and talented members to help us achieve this goal! If you enjoy beign a part of a fast-paced team, look no further!
+            </p> 
+
+            <p>
+                Applications for the 2017 season will open near the beginning of the spring semester. If you are selected, interviews to join the team will follow the applications. We currently plan to have the entire process completed within the first three weeks of the semester. 
+            </p>
+            <p>
+                The application will be on this page when it is released! In the meantime, feel free to learn more about ROV on <a href="index.php">our main page</a>
+            </p>
+            <img src="/rov/images/ROVMaelstrom.jpg" alt="Maelstrom" style="width:100%" style="float:center" style="display:block">
+                <p class="text-center">2015-2016 ROV <em>Maelstrom</em></p>
+        
+
+        </div>
+         
+    </div>
+</div>
+<!-- /.well -->
+
+<?php include '../footer.php';?>
