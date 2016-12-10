3040e5cca2c717966d70c32ca5112a123c89a946
Matt Molo <molo.matt@gmail.com>
2016-12-10 15:14:56 -0500

    Fixing the redirect because ROV has good SEO

--- committees/rov/index.php
+++ committees/rov/index.php
@@ -1,4 +1,5 @@
 <?php
     /* THIS IS DEPRECATED MATERIAL, ONLY PRESENT FOR LEGACY URLS. */
-    header("Location: ../rov/")
+    header("HTTP/1.1 301 Moved Permanently"); 
+    header("Location: https://purdueieee.org/rov/"); 
 ?>
