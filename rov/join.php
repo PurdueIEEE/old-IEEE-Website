f0783e59e2b8cd2b3f2a458f4dc0c307d82e2fe1
Kyle Rakos <krakos@purdue.edu>
2017-01-08 23:22:16 -0500

    Added ROV application to page

--- rov/join.php
+++ rov/join.php
@@ -24,17 +24,18 @@
 
             <h2 class="text-center">Join ROV</h2>
             <p>
-                The ROV team brings together expertise in electronics, mechanics, and software in order to create an unrivled vehicle. We intend to finish first place in the world, and we are therefore looking for dedicated and talented members to help us achieve this goal! If you enjoy beign a part of a fast-paced team, look no further!
+                The ROV team brings together expertise in electronics, mechanics, and software in order to create an unrivled vehicle. We intend to finish first place in the world, and we are therefore looking for dedicated and talented members to help us achieve this goal! If you enjoy beign a part of a fast-paced team and learn quickly look no further. We are diving into the build season and could use more help - especially electrically!
             </p> 
 
             <p>
-                Applications for the 2017 season will open near the beginning of the spring semester. If you are selected, interviews to join the team will follow the applications. We currently plan to have the entire process completed within the first three weeks of the semester. 
+                Applications for the 2017 season are now open and will be open until Saturday 1/14 at midnight. If you are selected, interviews to join the team will follow the week of 1/15 and decisions will be made by Saturday 1/21. If you are unsure if you want to join yet, come to our first meeting on Saturday 1/14 at 1:00pm in POTR 234 to learn more!
             </p>
             <p>
-                The application will be on this page when it is released! In the meantime, feel free to learn more about ROV on <a href="index.php">our main page</a>
+                The application can be filled out below or in a seperate window <a href="https://docs.google.com/forms/d/e/1FAIpQLSfEaGVV0-U__d_36NcVh8CiLHAl_Wko2C_AdA620L1b94Xt8A/viewform">here</a>.
             </p>
-            <img src="/rov/images/ROVMaelstrom.jpg" alt="Maelstrom" style="width:100%" style="float:center" style="display:block">
-                <p class="text-center">2015-2016 ROV <em>Maelstrom</em></p>
+            <p>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfEaGVV0-U__d_36NcVh8CiLHAl_Wko2C_AdA620L1b94Xt8A/viewform?embedded=true" width="700" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+            </p>   
         
 
         </div>
