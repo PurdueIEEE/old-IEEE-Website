3e9a1aec90254f146f1d8562bb5cbe6f715f4904
Matt Molo <molo.matt@gmail.com>
2017-01-10 19:11:59 -0600

    fixing date issue...

--- events.php
+++ events.php
@@ -1,2 +1,2 @@
-<strong>Callout Wednesday, 1/8 in EE 129 at 6:30!</strong>
-<p>Come join to figure out more about IEEE, its committees, and have some free food and drink!</p>
+<strong>Callout Wednesday, 01/11 in EE 129 at 6:30!</strong>
+<p>Come join to figure out more about IEEE, its committees, and have some free food and drinks!</p>
