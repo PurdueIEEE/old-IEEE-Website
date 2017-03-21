55a6c58052c2b33f1fac2674eb246aeb2321ddf4
Kyle Rakos <krakos@purdue.edu>
2017-03-20 22:04:07 -0400

    Added codecafe folder

---
+++ codecafe/index.php
@@ -0,0 +1,77 @@
+<?php
+    $page_title = 'Home';
+    $site_title = 'Code Cafe!';
+    $site_index = '/codecafe';
+    include 'header.php';
+?>
+
+<!-- Content Row -->
+<div class="well card-1">
+    <div class="row">
+        <div class="col-md-5 text-dark col-md-offset-1">
+        	<h1 class="text-center">IEEE Code Cafe</h1>
+        	<h2 class="text-center">Saturday, 25th March, 2017</h2>
+        	<h2 class="text-center">HICKS G980D</h2>
+        </div>
+
+        <div class="col-md-5 text-dark col-md-offset-1">
+        	<p class="text-center"><img src="codecafe1.jpg" alt="Code Cafe Mentorship" style="width:80%" style="float:center" style="display:block"></p>
+        </div>
+	</div>
+
+	<div class="row">
+        <div class="col-md-5 text-dark col-md-offset-1">
+        	<p class="text-center"><img src="logo.jpg" alt="Code Cafe Logo" style="width:80%" style="float:center" style="display:block"> </p>
+        </div>
+
+        <div class="col-md-5 text-dark col-md-offset-1">
+       	<h2>What is Code Cafe?</h2>
+
+
+        	<p class="text-justified">Code cafe is an open learning event made to help you get started with learning how to code. At this event, we want you to be as relaxed as possible. There will be free beverages (it is a cafe after all), mugs, and refreshments.</p>
+        </div>
+	</div>
+
+	<div class="row">
+        <div class="col-md-5 text-dark col-md-offset-1">
+        	<h1 class="text-center">How is Code Cafe structured?</h1>
+        	<p class="text-justify">During the five hours of Code Cafe, you will be divided in groups first, based on your current programming level and the skills you want to learn.</p>
+
+			<p class="text-justify">The concepts will range from variables, loops, and conditionals to WebGL-based game programming, and scientific computing techniques. The primary language is JavaScript, selected based on ease of use and impact. Mentors will be guiding attendees on a global and individual basis.</p>
+        </div>
+
+        <div class="col-md-5 text-dark col-md-offset-1">
+        	<p class="text-center"><img src="codecafe1.jpg" alt="Code Cafe Mentorship" style="width:80%" style="float:center" style="display:block"></p>
+        </div>
+	</div>
+
+	<div class="row">
+        <div class="col-md-5 text-dark col-md-offset-1">
+        	<p class="text-center"><img src="codecafe2.jpg" alt="Code Cafe Mentorship" style="width:80%" style="float:center" style="display:block"> </p>
+        </div>
+
+        <div class="col-md-5 text-dark col-md-offset-1">
+        	<h1 class="text-center">What to bring?</h1>
+        	<p class="text-justified">An equipment to write code, preferably a laptop or portable machine and the curiosity to be the Picasso of the art of programming.</p>
+        </div>
+	</div>
+
+	<div class="row">
+        <div class="col-md-5 text-dark col-md-offset-1">
+        	<h1 class="text-center">How can I start?</h1>
+        	<p class="text-justified">Click on the sign up link on the top right to RSVP, or just rather click here</p>
+        </div>
+
+        <div class="col-md-5 text-dark col-md-offset-1">
+        	<h1 class="text-center">I have more questions</h1>
+        	<p class="text-justified">Feel free to shoot us an email at codecafe@purdueieee.org
+
+</p>
+        </div>
+	</div>
+
+
+</div>
+<!-- /.well -->
+
+<?php include '../footer.php';?>
