4bf9c5716b8635a79986e8caa0a4ea3d5e9946b4
Brandon Stewart <stewartb2@gmail.com>
2017-03-21 15:58:06 -0400

    Update Racing Page

--- racing/index.php
+++ racing/index.php
@@ -13,7 +13,7 @@
             <div class="col-md-8 text-dark col-md-offset-2 text-dark">
                 <h2>Racing Team</h2>
                 <p> The IEEE Racing Team builds an electric go-kart to compete in evGrandPrix. In addition to go-karts, the team undertakes small projects such as building an off-road buggy to explore the field of electric motorsports even further.</p>
-                <p>Contact <a href="mailto:jcourtn@purdue.edu">John Courtney</a> for more details.</p>
+                <p>Contact <a href="mailto:efiguer@purdue.edu">Erick Figueroa</a> for more details.</p>
             </div>
         </div>
     </div>
