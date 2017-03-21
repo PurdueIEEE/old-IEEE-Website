084c22d79f642fbcb798424b4af9b57611e2d74b
Brandon Stewart <stewartb2@gmail.com>
2017-03-21 12:20:43 -0400

    Added more info about code cafe

--- events.php
+++ events.php
@@ -1,4 +1,8 @@
 <strong> CODE CAFE </strong>
 <p> Learn about programming and web development at Code Cafe. There will be be free refreshments provided.</p>
+<strong> Date: Saturday 03/25 </strong>
+<strong> Time: 1pm - 6pm </strong>
+<strong> Place: HICKS G980D</strong>
+
 <strong> <a href="http://codecafe.purdueieee.org/signup">Signup Here</a> </strong>
 
