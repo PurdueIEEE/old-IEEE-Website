8af1cdc537db31fd5230c01b6194bfab6202ee66
Brandon Stewart <stewart@stewart.local>
2017-05-22 22:51:00 -0400

    Started work on updating website for officers of 2017

---
+++ officers/2017.php
@@ -0,0 +1,251 @@
+<?php
+    $page_title = '2016 Officers';
+    $site_title = 'Purdue IEEE';
+    $site_index = '/';
+    $root = true;
+    include '../header.php';
+?>
+
+    <div class="well card-1 text-dark">
+
+        <!--- Pagination -->
+        <div class="row text-center">
+            <div class="col-lg-12">
+                <ul class="pagination">
+                    <?php
+                        $year = 2016;
+                        include 'header.php';
+                    ?>
+                </ul>
+            </div>
+        </div>
+
+        <hr />
+
+        <!-- President -->
+        <div class="row">
+
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Aditya.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Sanay Shah</h3>
+                <h4>President</h4>
+                <p>Aditya Vaidyam is a Senior in Neurobiology &amp; Physiology, as well as Computer Science, that joined Purdue IEEE in Fall 2014. He co-founded Purdue's Engineering in Medicine and Biology Society in 2015. He aims to study medicine and focus on primary care, while infusing engineering techniques into its practice. Outside of academics, Aditya enjoys both listening to and producing music, cycling, programming, home automation, and playing video games. Aditya is scheduled to graduate Spring 2017.</p>
+                <p><em>Committee Involvement</em>: <a href="/embs/">EMBS</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Vice President -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Sanay_Shah.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Brandon Stewart</h3>
+                <h4>Vice President</h4>
+                <p>Sanay Shah is a Junior in Industrial Engineering. He joined IEEE in Fall 2014 as a Mechanical Team member on the Purdue IEEE ROV Team. Since joining IEEE, Sanay has worked on the ROV Mechanical and Administrative teams, as well as being the ROV Captain during the Spring 2016 semester. His technical and administrative passions continue to grow as he focuses on on the success of all the technical committees. As Vice President, Sanay hopes to increase communication between committees to improve technical knowledge of all members. He also aims to work with other committees to build a friendlier campus wide atmosphere. He has had internships at RSS Grinders &amp; Automation Inc. as a machinist operating manual and CNC machinery. He also enjoys playing and watching sports and is a huge fan of The Chicago Blackhawks.</p>
+                <p><em>Committee Involvement</em>: <a href="/rov/">ROV</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Treasurer -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Bobby_Zhang.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Michael Anderson</h3>
+                <h4>Treasurer</h4>
+                <p>Bobby is a Sophomore in Computer Science and Mathematics with a minor in Statistics. He joined Purdue IEEE as a member of Learning Community in Spring of 2016. He likes to challenge himself so as to know and break the limit. Passionate about IT industry, especially Artificial Intelligence and Web Development, he plans to pursue Machine Intelligence and Systems Programming tracks in Computer Science. He also hopes to apply the programming knowledge on Engineering fields. With interest in data and finance, he is now undertaking the role of IEEE Treasurer, expecting to simplify the reimbursement process and the account documents with the help of a newly developed system.</p>
+                <p><em>Committee Involvement</em>: <a href="/committees/old_learning/">Learning</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Secretary -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Brandon_Stewart.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Jessica Chin</h3>
+                <h4>Secretary</h4>
+                <p>Brandon is a Sophomore in Computer Science. He joined IEEE in the Fall of 2015 on the ROV Team. He plans to pursue the Systems Programming and Computer Graphics and Visualization tracks in Computer Science. In his free time, he enjoys running, programming, and watching movies. As Secretary, he plans to promote stronger communication among teams and will focus on ensuring members are informed of IEEE events.</p>
+                <p><em>Committee Involvement</em>: <a href="/csociety/">Computer Society</a>, <a href="/rov/">ROV</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Professional Committee Head -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Yash_Nain.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Reshef Elisha</h3>
+                <h4>Professional Committee Head</h4>
+                <p>Yash is a junior in Multidisciplinary Engineering. He joined IEEE in the fall of 2014 to work on the Aerial Robotics committee. He hopes to combine his engineering knowledge with a business background. As professional head, he hopes to work with companies, professionals, and members to ensure that everyone gets the best deal they can, while also hoping to integrate better opportunities for education whenever possible. In his free time, he is a movie buff and enjoys cooking, playing and watching sports, and playing video games.</p>
+                <p><em>Committee Involvement</em>: <a href="/partieee/">PARTIEEE</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Learning Committee Head -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Sophia_Duan.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Justin Joco</h3>
+                <h4>Learning Committee Head</h4>
+                <p>Sophia is a Sophomore pursuing a major in Electrical Engineering and minor in Computer Science and Mathematics. She studied classical ballet and traditional dances in her hometown of Beijing, China. Apart from being Learning Head, she is also a member of the ROV Software and Mechanical Teams. Without having much technical knowledge before joining Purdue IEEE, Sophia tried her best to learn new things and experience a new culture in her freshman year and wants to help more international students get involved. She loves laughing and hopes her smile will always bring positive energy to others. Sophia’s hobbies also include playing the piano and violin, running, and reading.</p>
+                <p><em>Committee Involvement</em>: <a href="/rov/">ROV</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Social Committee Head -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Rodolfo_Leiva.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Yash Nain</h3>
+                <h4>Social Committee Head</h4>
+                <p>Rodolfo is a Senior in Electrical Engineering. He joined Purdue IEEE in the Fall of 2014 to be part of the ROV team. He attributes the IEEE Purdue Chapter as his main source of technical knowledge and is still learning a lot from the organization to this day. Rodolfo is very excited to prepare social activities to unite students within IEEE and all fields of engineering and careers. In his free time, Rodolfo likes discovering new music, watching movies and hanging out with his friends. He’s always looking for new, fun activities he can put together for the organization, so if you have any send them his way.</p>
+                <p><em>Committee Involvement</em>: <a href="/rov/">ROV</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Committee Chair #1 -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Matt.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Raghav Malik</h3>
+                <h4>Computer Society Chair</h4>
+                <p>Matt is a Senior in Computer Science and a former President of Purdue IEEE Student Organization, one of few CS majors to have risen to that office in the organization's history. Growing up in Joliet, IL, Matt has been working with robotics and computers since his involvement on his high school FIRST Robotics team. He is following the Systems Programming track in computer science. Outside of academics, he enjoys web development and video games. Matt had his first experience with IEEE during Summer 2013 before he had even started classes at Purdue. He was integral to developing the software on the ROV team, and has since risen to the role of Software Team Lead on the team. Back during his second year at Purdue, Matt has become extremely involved in all aspects of the student organization. In addition to his responsibilities as President and on the ROV team, he served as the Computer Society Secretary. As President, Matt encouraged the growth of the five committees at the time, built a strong network of motivated students, pushed for wider involvement of all majors within IEEE, and oversaw successful competition seasons from the technical committees. For Matt, retirement takes the form of reviving the IEEE Computer Society as the Computer Society President and pushing for a wider array of challenging software projects.</p>
+                <p><em>Committee Involvement</em>: <a href="/csociety/">Computer Society</a>, <a href="/rov/">ROV</a>, <a href="/committees/old_learning/">Learning</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Committee Chair #2 (Fall) -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/EMBS_Anjali_Malik.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Apoorva Bhagwat</h3>
+                <h4>Engineering in Medicine and Biology Society (EMBS) Chair (Fall)</h4>
+                <p>Anjali is a Senior in Weldon School of Biomedical Engineering at Purdue. She has been a member and supporter of IEEE and has co-founded the EMBS committee aiming to spread the importance of BME while building a platform for student innovation in medicine and biology. As the committee chair, she wishes to build a network of students and supporters who actively participate and contribute to development and promotion of this field, while focusing on creation of confident skill sets for the committee's student members. She is passionate about applying her integrated engineering skills towards various real life problems and is planning to pursue graduate school. Her interests include spending time with family and friends, playing badminton, cooking and reading.</p>
+                <p><em>Committee Involvement</em>: <a href="/embs/">EMBS</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Committee Chair #3 -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Michael_Hayashi_2016.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Zach Vander Missen</h3>
+                <h4>IEEE Microwave Theory &amp; Techniques Society (MTT-S) Chair</h4>
+                <p>Michael is a Graduate Student in Electrical Engineering currently working on his Master's degree. Growing up in the Caterpillar community of Peoria, IL, Michael has known he wanted to be an electrical engineer since childhood. He is a first-generation Boilermaker who loves the opportunities Purdue has given him. Michael has surveyed a wide range of topics in electrical engineering including Fields and Optics, Microelectronics and Nanotechnology, and some Computer Engineering. His current research is in Power and Energy Systems concerning the analysis and mitigation of common-mode current in hybrid vehicle drives. Outside of academics, he enjoys reading, jogging, and video games. Michael first became active with IEEE in Spring 2012 through his participation on the ROV team. Emerging from post-presidency retirement from Purdue IEEE leadership, Michael took it upon himself to expand the MTT-S Chapter at Purdue in his sixth year on campus. He also volunteers with the Central Indiana Section of IEEE, where he is Section Vice Chair and Educational Activities Chair after passing on the role of Section Student Representative.</p>
+                <p><em>Committee Involvement</em>: Conference, <a href="/csociety/">Computer Society</a>, <a href="/committees/old_learning/">Learning</a>, <a href="/mtts/">IEEE MTT-S</a>, <a href="/partieee/">PARTIEEE</a>, <a href="/rov/">ROV</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Committee Chair #4 (Fall) -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Michael_Malachowski.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Ellie Topi</h3>
+                <h4>Purdue Aerial Robotics Team IEEE Captain (Fall)</h4>
+                <p>Michael is a Senior in Electrical Engineering. He joined the Purdue IEEE Aerial Robotics Team in the Fall of 2014, eventually taking on the role of electrical team lead, and is currently the Aerial Robotics team captain. He is passionate about designing and working with embedded systems, and is adamant about leading the team in designing a fully custom UAS solution. In his free time he enjoys playing video games, target shooting, and programming.</p>
+                <p><em>Committee Involvement</em>: <a href="/partieee/">PARTIEEE</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Committee Chair #5 -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Racing_John.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Adrian White</h3>
+                <h4>Racing Committee Co-Chair</h4>
+                <p>John Courtney is a Senior in Mechanical Engineering. Prior to joining Purdue IEEE, he was actively involved in the Purdue EPICS (Engineering Projects in Community Service) Program. He joined Purdue IEEE in Fall 2013 where he played the role of Electrical Lead for the Racing Committee. Since Fall 2014, he has worked as Captain of the Racing Committee. John is strongly interested in motorsports and the automotive industry. His other interests include going rock climbing, travelling, and fishing.</p>
+                <p><em>Committee Involvement</em>: <a href="/racing/">Racing</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Committee Chair #6 -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Erick_Figueroa.png')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Erick Figueroa</h3>
+                <h4>Racing Committee Co-Chair</h4>
+                <p> Erick Figueroa is a Sophomore in Electrical Engineering Technology and with a minor in Spanish and Entrepreneurship Certificate. He joined IEEE in the Fall of 2015 as a member of the Racing Committee. As a first generation university student who appreciates the opportunities Purdue has offered him, he actively pursues to stay involved with campus activities and organizations. He has a wide range of experiences from an Electrical Engineering to Leadership and Public Speaking. He is a very energetic and positive individual who always looks on the bright side and takes pride in his work ethic. In his spare time, he loves to work out and socialize and spend time with family and friends. He is currently on track to graduate in Spring 2019. </p>
+                <p><em>Committee Involvement</em>: <a href="/racing/">Racing</a></p>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Committee Chair #7 -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="officer-cropped"
+                    style="background-image: url('/images/officers/Kyle.jpg')">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h3>Michael Hayashi</h3>
+                <h4>Remotely Operated underwater Vehicle (ROV) Captain</h4>
+                <p>Kyle Rakos is a Senior in Computer Engineering. He joined IEEE at the beginning of his freshman year by taking on the role of Sponsorship Coordinator of the Purdue IEEE ROV Team. His technical and administrative passions continue to grow as he took on role of the Industrial Relations Head of IEEE and Captain of the ROV team. During these roles he interacts with companies on behalf of IEEE and organizes the ROV team in the MATE International ROV Competition. Feeling the desire to improve upon his 15th Place finish the previous season, he is devoting all his time to being the Team Captain for ROV. He also enjoys playing sports and is interested in space.</p>
+                <p><em>Committee Involvement</em>: <a href="/rov/">ROV</a></p>
+            </div>
+        </div>
+
+    </div>
+
+<?php include '../footer.php'; ?>
