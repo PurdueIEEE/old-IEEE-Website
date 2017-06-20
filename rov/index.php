d668a1c0c5bd4c72ff197aea85ca2a8d7fb75b8d
Kyle <rakoskr6@users.noreply.github.com>
2017-06-20 17:03:47 -0400

    Added pre-competition ROV update and new pictures (#100)

--- rov/index.php
+++ rov/index.php
@@ -23,16 +23,26 @@
         <div class="col-md-7 text-dark col-md-offset-1">
             <h2 class="text-center">Remotely Operated underwater Vehicle (ROV) Team</h2>
             <br>
-            <p>
-                The ROV Team is currently preparing for the International Competition this June! More info coming soon!
+            
+            <div class="col-md-6">
+                <p>
+                The ROV Team is competing in the MATE International Competition June 23-25. Read our pre-competition <a href="files/June2017Newsletter.pdf">newsletter here</a> and follow us live at <a href="https://www.marinetech.org/live-videostream">MATE's website.</a> We are proud to present our ninth ROV in Long Beach, CA, ROV <em>Cephalopod!</em> Feel free to check out <a href="https://drive.google.com/open?id=0B7b8J6sbp7ReSkxCQ21xa051REk">our spec sheet</a>. We will release more of our documents and pictures after the competition!
             </p>
-          
+            </div>
+
+            <div class="col-md-6">
+                <img src="/rov/images/ROV_Cephalopod_isolated.png" alt="Cephalopod" style="width:80%" style="float:left" style="display:block">
+                <p class="text-center">ROV <em>Cephalopod</em> Picture</p>
+            </div>
+
+
+
             <br>
 
             <h2 class="text-center">About The ROV Team</h2>
             <div class="col-md-6">
-                <img src="/rov/images/ROVMaelstrom.jpg" alt="Maelstrom" style="width:100%" style="float:left" style="display:block">
-                <p class="text-center">2015-2016 ROV <em>Maelstrom</em></p>
+                <img src="/rov/images/Cephalopod_Render.jpg" alt="Maelstrom" style="width:100%" style="float:left" style="display:block">
+                <p class="text-center">ROV <em>Cephalopod</em> Render</p>
             </div>
 
             <div class="col-md-6">
@@ -54,100 +64,11 @@
             </p>
 
 
-            <h2 class="text-center">2016-2017 Team Pictures</h2>
-            <h3 class="text-center">Full Team Picture</h3>
-            <p class="text-center"><img src="/rov/images/team_fall_2016.jpg" alt="Full Team" style="width:100%"></p><br>
-
-            <h3 class="text-center">Electrical Team</h3>
-            <p class="text-center"><img src="/rov/images/electrical_team_spring_2017.jpg" alt="Electrical Team" style="width:100%"></p><br>
-
-
-            <div class="row">
-                <div class="col-sm-2"><p class="text-center">Rodolfo<br>Leiva</p></div>
-                <div class="col-sm-1"><p class="text-center">Alan<br>Han</p></div>
-                <div class="col-sm-2"><p class="text-center">Nick<br>Loffredo</p></div>
-                <div class="col-sm-2"><p class="text-center">Jason<br>King</p></div>
-                <div class="col-sm-2"><p class="text-center">Ryan<br>McBee</p></div>
-                <div class="col-sm-1"><p class="text-center">James<br>Scholl</p></div>
-                <div class="col-sm-2"><p class="text-center">Luke<br>McBee</p></div>
-            </div>
+            <h2 class="text-center">2016-2017 Team Picture</h2>
+            <p class="text-center"><img src="/rov/images/2017FinalTeamPicture.png" alt="2017 Team" style="width:100%"></p><br>
+            
+            <p><a href="2017otherpictures.php">See individual technical team pictures here</a></p>
             <br>
-            <div class="row">
-                <div class="col-sm-1"><p class="text-center"><br></p></div>
-                <div class="col-sm-1"><p class="text-center"><br></p></div>
-                <div class="col-sm-2"><p class="text-center">Jessica<br>Chin</p></div>
-                <div class="col-sm-2"><p class="text-center">Carolyn<br>Lewelling</p></div>
-                <div class="col-sm-2"><p class="text-center">David<br>Bustamante</p></div>
-                <div class="col-sm-2"><p class="text-center">Sreyas<br>Mirthipati</p></div>
-                <div class="col-sm-2"><p class="text-center"><br></p></div>
-            </div>
-            <div>
-                <p>Not Pictured: Samuel Deghuee, Kyla Hayes, David Bustamante, John Brankamp, Nick Loffredo, Richard Shen, Romita Biswas, &amp; Xi Wu</p>
-            </div>
-
-
-            <h3 class="text-center">Mechanical Team</h3>
-            <p class="text-center"><img src="/rov/images/mechanical_team_spring_2017.jpg" alt="Mechanical Team" style="width:100%"></p><br>
-            <div class="row">
-                <div class="col-sm-2"><p class="text-center">Amandeep<br>Singh</p></div>
-                <div class="col-sm-2"><p class="text-center">Rohit<br>Srivastava</p></div>
-                <div class="col-sm-2"><p class="text-center">Alex<br>Gebhardt</p></div>
-                <div class="col-sm-1"><p class="text-center">Joe<br>Pejril</p></div>
-                <div class="col-sm-2"><p class="text-center">Jacob<br>Cottrell</p></div>
-                <div class="col-sm-2"><p class="text-center">Jonathan<br>Kline</p></div>
-                <div class="col-sm-1"><p class="text-center">Reed<br>Trende</p></div>
-            </div>
-            <br>
-
-            <div class="row">
-                <div class="col-sm-2"><p class="text-center">Seth<br>Emond</p></div>
-                <div class="col-sm-2"><p class="text-center">Enlee<br>Choo</p></div>
-                <div class="col-sm-2"><p class="text-center">Monica<br>Mah</p></div>
-                <div class="col-sm-1"><p class="text-center">Logan<br>Walters</p></div>
-                <div class="col-sm-2"><p class="text-center">Ben<br>Hutchins</p></div>
-                <div class="col-sm-2"><p class="text-center"><br></p></div>
-                <div class="col-sm-1"><p class="text-center">Tyler<br>Stagge</p></div>
-            </div>
-            <div>
-                <p>Not Pictured: Joshua Berg, Alex Ruffino, Teal Dowd, Dan Schillizi, Katie Mao, Jacob Cottrell, &amp; Bayley Clausen </p>
-            </div>
-
-
-            <h3 class="text-center">Software Team</h3>
-            <p class="text-center"><img src="/rov/images/software_team_spring_2017.jpg" alt="Software Team" style="width:100%"></p><br>
-            <div class="row">
-                <div class="col-sm-2"><p class="text-center">Andrew<br>Bostock</p></div>
-                <div class="col-sm-2"><p class="text-center">Kartik<br>Yadav</p></div>
-                <div class="col-sm-2"><p class="text-center">Brandon<br>Stewart</p></div>
-                <div class="col-sm-2"><p class="text-center">Samuale<br>Yigrem</p></div>
-                <div class="col-sm-2"><p class="text-center">Ian<br>Sibley</p></div>
-                <div class="col-sm-2"><p class="text-center">Zidaan<br>Dutta</p></div>
-            </div>
-            <br>
-
-            <div class="row">
-                <div class="col-sm-2"><p class="text-center"><br></p></div>
-                <div class="col-sm-2"><p class="text-center">Yuqin<br>Duan</p></div>
-                <div class="col-sm-2"><p class="text-center"><br></p></div>
-                <div class="col-sm-2"><p class="text-center"><br></p></div>
-                <div class="col-sm-2"><p class="text-center">Kendal<br>Norman</p></div>
-                <div class="col-sm-2"><p class="text-center"><br></p></div>
-            </div>
-            <div>
-                <p>Not Pictured: Matthew Molo, Ben Maxfield, Mimansa Verma, Kendal Norman, Dominic Miller, Keith Aylwin, Dimple Dhawan, Ajay Gopakumar, &amp; Charles Li </p>
-            </div>
-
-            <h3 class="text-center">Administrative Team</h3>
-            <p class="text-center"><img src="/rov/images/administrative_team_fall_2016.jpg" alt="Administrative Team" style="width:100%"></p><br>
-            <div class="row">
-                <div class="col-sm-2"><p class="text-center">Matt Molo<br><em>Fall Software Lead</em></p></div>
-                <div class="col-sm-2"><p class="text-center">Joshua Berg<br><em>Fall Mechanical Lead</em></p></div>
-                <div class="col-sm-2"><p class="text-center">Carolyn Lewelling<br><em>Sponsorship Coordinator</em></p></div>
-                <div class="col-sm-2"><p class="text-center">Alex Ruffino<br><em>Spring Mechanical Lead</em></p></div>
-                <div class="col-sm-2"><p class="text-center">Kyle Rakos<br><em>Captain</em></p></div>
-                <div class="col-sm-2"><p class="text-center">Luke McBee<br><em>Electrical Lead</em></p></div>
-            </div>
-            <p>Not Pictured: Sanay Shah <em>(Vice-Captain)</em>, Ben Maxfield <em>(Spring Software Lead)</em>, &amp; Henry Shi <em>(Technical Writter)</em></p>
 
             <p class="text-center"><a href="https://www.facebook.com/purdueieeerov"><i class="fa fa-facebook-square fa-4x"></i></a></p>
             <br>
--- rov/updates/index.php
+++ rov/updates/index.php
@@ -25,32 +25,53 @@
             <p>Here is a list of updates that have been posted on the ROV website! You can also find an overview about our previous seasons under the history tab to the left.</p>
             <hr>
 
+            <h3>Pre-Competition Update</h3>
+            <p><em>June 20, 2017</em></p>
+             <div class="col-md-8">
+                 <p class="text-justify">
+                 The ROV Team is competing in the MATE International Competition June 23-25. Read our pre-competition <a href="../files/June2017Newsletter.pdf">newsletter here</a> and follow us live at <a href="https://www.marinetech.org/live-videostream">MATE's website.</a> We are proud to present our ninth ROV in Long Beach, CA, ROV <em>Cephalopod!</em> Feel free to check out <a href="https://drive.google.com/open?id=0B7b8J6sbp7ReSkxCQ21xa051REk">our spec sheet</a>. We will release more of our documents and pictures after the competition!
+                 </p>
+             </div>
+             <div class="col-md-4">
+                <img src="/rov/images/ROV_Cephalopod_isolated.png" alt="Cephalopod" style="width:80%" style="float:left" style="display:block">
+                <p class="text-center">ROV <em>Cephalopod</em> Picture</p>
+            </div>
+             <hr>
+
             <h3>Mid-Semester Update</h3>
             <p><em>March 2017</em></p>
-             <p class="text-justify">
-             The ROV Team is currently finishing up manufacturing and integration of key ROV components. Checkout our <b><a href=/rov/files/March2017Newsletter.pdf>March newsletter here.</a></b> We are well on our way to success in the <a href="http://www.marinetech.org/rov-competition-2/">MATE International ROV competition</a> in Long Beach, CA!
-             </p>
+            <div class="col-md-12">
+                 <p class="text-justify">
+                 The ROV Team is currently finishing up manufacturing and integration of key ROV components. Checkout our <b><a href=/rov/files/March2017Newsletter.pdf>March newsletter here.</a></b> We are well on our way to success in the <a href="http://www.marinetech.org/rov-competition-2/">MATE International ROV competition</a> in Long Beach, CA!
+                 </p>
+            </div>
              <hr>
 
             <h3>Happy New Year!</h3>
             <p><em>January 2017</em></p>
-             <p class="text-justify">
-             The ROV Team began its ninth season with over 50 members! The team has made tremendous progress on the ROV designs the first semester and are now eagerly ready to manufacture the ROV. Checkout our  <b><a href=/rov/files/Fall2016Newsletter.pdf> our newsletter here.</a></b> We are well on our way for the 2017 competition in Long Beach, CA. Designs for the <a href="http://www.marinetech.org/rov-competition-2/">MATE competition!</a>
-             </p>
+            <div class="col-md-12">
+                 <p class="text-justify">
+                 The ROV Team began its ninth season with over 50 members! The team has made tremendous progress on the ROV designs the first semester and are now eagerly ready to manufacture the ROV. Checkout our  <b><a href=/rov/files/Fall2016Newsletter.pdf> our newsletter here.</a></b> We are well on our way for the 2017 competition in Long Beach, CA. Designs for the <a href="http://www.marinetech.org/rov-competition-2/">MATE competition!</a>
+                 </p>
+            </div>
              <hr>
 
              <h3>New Season</h3>
             <p><em>August 2016</em></p>
-             <p class="text-justify">
-              Interested in joining the team this year? Come to our first meeting on Wednesday, August 31 at 5:30pm in Hampton 1144! We have lots of exciting plans for this year and hope to finish first in 2017! You will be asked to fill out <a href="https://docs.google.com/forms/d/e/1FAIpQLSdPntz4UBPkGUmPngaLSoxhe6kaNKxhHUveHme7nMFi-du0HQ/viewform">this brief application to join</a>
-            </p>
+            <div class="col-md-12">
+                 <p class="text-justify">
+                  Interested in joining the team this year? Come to our first meeting on Wednesday, August 31 at 5:30pm in Hampton 1144! We have lots of exciting plans for this year and hope to finish first in 2017! You will be asked to fill out <a href="https://docs.google.com/forms/d/e/1FAIpQLSdPntz4UBPkGUmPngaLSoxhe6kaNKxhHUveHme7nMFi-du0HQ/viewform">this brief application to join</a>
+                </p>
+            </div>
             <hr>
 
             <h3>2016 Competition</h3>
             <p><em>June 2016</em></p>
-             <p class="text-justify">
-             Purdue ROV competed in Houston, TX from June 22 - 25, 2016 at NASA's Neutral Buoyancy Lab! Thanks for all who supported our team at the competition and throughout the year! We had a wonderful technical presentation, an excellent poster, and great technical documentation. We had some electronics issues during the mission runs but were still able to place 13th internationally! Additionally, Kyle Rakos was awarded with the Flying Fish award for his passion and demonstrated leadership in the field!  We will soon add more details under Past Seasons and Pictures!
-            </p>
+            <div class="col-md-12">
+                 <p class="text-justify">
+                 Purdue ROV competed in Houston, TX from June 22 - 25, 2016 at NASA's Neutral Buoyancy Lab! Thanks for all who supported our team at the competition and throughout the year! We had a wonderful technical presentation, an excellent poster, and great technical documentation. We had some electronics issues during the mission runs but were still able to place 13th internationally! Additionally, Kyle Rakos was awarded with the Flying Fish award for his passion and demonstrated leadership in the field!  We will soon add more details under Past Seasons and Pictures!
+                </p>
+            </div>
             <hr>
 
 
