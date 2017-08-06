352419442ed41eb7c95f04ca1b3de1151c2007fb
Jessica Chin <chin21@purdue.edu>
2017-08-05 22:21:11 -0600

    Edit :Description

--- racing/index.php
+++ racing/index.php
@@ -12,10 +12,10 @@
             <!-- Content Column -->
             <div class="col-md-8 text-dark col-md-offset-2 text-dark">
                 <h2>Racing Team</h2>
-                <p> IEEE Racing builds an electric go-kart that competes in the Purdue evGrandPrix that takes place annually at the Indianapolis Motor Speedway. We are looking for members to redesign electrical systems, get sponsors and build industry contacts, improve safety features, and most importantly, have a great time learning about electric vehicle technology. <br>
-
-Team meetings are once a week in the off-season and twice a week during race season. Meetings are broken into planning time where we discuss our project and budget and build sessions where members get hands-on with the go-kart. <br>
-
+                <p> IEEE Racing builds an electric go-kart that competes in the Purdue evGrandPrix that takes place annually at the Indianapolis Motor Speedway. We are looking for members to redesign electrical systems, get sponsors and build industry contacts, improve safety features, and most importantly, have a great time learning about electric vehicle technology. </p>
+<p>
+Team meetings are once a week in the off-season and twice a week during race season. Meetings are broken into planning time where we discuss our project and budget and build sessions where members get hands-on with the go-kart. </p>
+<p>
 All majors are welcome!!
 </p>
                 <p>Contact <a href="mailto:efiguer@purdue.edu">Erick Figueroa</a> for more details.</p>
