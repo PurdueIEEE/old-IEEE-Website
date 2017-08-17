5e078a1f6b4f299eae4142bbd3b1e623386bfe0a
Brandon Stewart <stewartb2@gmail.com>
2017-08-17 09:28:52 -0400

    updated social page

--- social/index.php
+++ social/index.php
@@ -11,7 +11,35 @@
             <div class="col-md-8 text-dark col-md-offset-2 text-dark">
                 <h1>IEEE Social Committee</h1>
 
-                <p>Get involved and meet new members in IEEE's social committee!</p>
+                <p>
+                    The Social Committee of IEEE was founded back in 2016 with the goal to provide members with fun and
+                creative events. Over its lifetime, it has hosted many events including paintball and go karts.
+                </p>
+
+                <h2>2017 - 2018 Events</h2>
+
+                <p>
+
+                </p>
+
+                <h2>2016 - 2017 Events</h2>
+                <p>
+                <h3>Paintball</h3>
+                <br>
+                <h4>IEEE vs ASME paintball game this Saturday, October 1st at 2pm.</h4>
+
+                <h3>Ice Skating</h3>
+                <br>
+                <h4>Come down to the ice rink and skate with your friends on Saturday, February 4th.</h4>
+                <br>
+                <img src="./iceskating.jpg" alt= "Ice Skating" height=500px />
+
+                <h3>Go Karts</h3>
+                <h4>Race against your teammates in Indianapolis on Saturday, April 1st.</h4>
+
+                <h3>Picnic</h3>
+                <h4>IEEE will be hosting picnic with free food on Friday, April 21st.</h4>
+                </p>
 
             </div>
         </div>
