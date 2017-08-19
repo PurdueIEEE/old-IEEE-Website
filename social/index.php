1ae4b68127877e223534a8064bde89cfca5332de
Brandon Stewart <stewartb2@gmail.com>
2017-08-19 12:28:37 -0400

    Fix social images going off side on mobile

--- social/index.php
+++ social/index.php
@@ -23,7 +23,7 @@
                 <br>
                 <h4>Graduate students can be nearly invisible on campus, but IEEE certainly wants to give them something to do this summer. Come to Jake's Roadhouse on Wednesday, July 5th at 6:00 PM to commiserate over the grad student life, find out if graduate school in ECE is a good option for you, or hear from Dr. Joseph Rispoli about what it takes to graduate again. Jake's trivia follows afterwards.</h4>
                 <br>
-                <img src="./images/grad_student_night.jpg" alt="Grad Student Night" height=500px />
+                <img src="./images/grad_student_night.jpg" alt="Grad Student Night" height=500px style="width: 100%"/>
                 </p>
 
                 <h2>2016–2017 Events</h2>
@@ -32,43 +32,43 @@
                 <br>
                 <h4>An IEEE vs ASME paintball game was held at the Paintball Barn in Attica, IN on Saturday, October 1st at 2:00 PM. This was a highlight of the IEEE member experience that allows members to vent some frustrations at committee leadership.</h4>
                 <br>
-                <img src="./images/paintball.jpg" alt="Paintball Game" height=500px />
+                <img src="./images/paintball.jpg" alt="Paintball Game" height=500px style="width: 100%"/>
                 
                 <h3>IEEE Day 2016</h3>
                 <br>
                 <h4>IEEE Day is the more important IEEE-themed holiday (the second is the IEEE Honor Ceremony Day), held annually on the first Tuesday in October. It celebrates the first time in history when engineers worldwide and IEEE members gathered to share their technical ideas in 1884. For 2016, Purdue IEEE Student Branch celebrated by holding a relaxing game night for the members with pizza and homemade cotton candy. Those in attendance enjoyed playing games like Rocket League and Fibbage.</h4>
                 <br>
-                <img src="./images/IEEE_Day_2016.jpg" alt="IEEE Day 2016" height=500px />
+                <img src="./images/IEEE_Day_2016.jpg" alt="IEEE Day 2016" height=500px style="width: 100%"/>
 
                 <h3>Kings Island Trip</h3>
                 <br>
                 <h4>The IEEE Social Committee tries to plan activities for members around during minor breaks. Some IEEE members who were on campus during October Break took a trip to Cincinatti, OH to visit Paramount Kings Island.</h4>
                 <br>
-                <img src="./images/kings_island.jpg" alt="Kings Island" height=500px />
+                <img src="./images/kings_island.jpg" alt="Kings Island" height=500px style="width: 100%"/>
 
                 <h3>IEEE vs. HKN Flag Football Game and BBQ</h3>
                 <br>
                 <h4>The Nice Guys of Eta Kappa Nu Beta Chapter and the members of Purdue Univ-West Lafayette IEEE Student Branch have a traditional game of flag football held once a year in which the winner takes home a trophy made from a gigantic hard drive. The 2016 match took place in Picket Park on November 6th. Despite a well-contested match, HKN takes the trophy home again. In a show of unity, both teams joined each other for a barbebue with s'mores.</h4>
                 <br>
-                <img src="./images/flag_football.jpg" alt="2016 IEEE vs. HKN Flag Football Game" height=500px />
+                <img src="./images/flag_football.jpg" alt="2016 IEEE vs. HKN Flag Football Game" height=500px style="width: 100%"/>
 
                 <h3>Game and Pizza Night</h3>
                 <br>
                 <h4>Twenty-five friends and acquaintences in IEEE gathered on January 22nd, 2017 to play video games, challenge each other in board games, and eat pizza on a Sunday when the dining courts were closed.</h4>
                 <br>
-                <img src="./images/game_night.jpg" alt="Spring 2017 Game Night" height=500 px />
+                <img src="./images/game_night.jpg" alt="Spring 2017 Game Night" height=500 px style="width: 100%"/>
 
                 <h3>Ice Skating</h3>
                 <br>
                 <h4>A few friends in IEEE drove down to the ice skating rink on Saturday, February 4th, 2017. A life behind textbooks might cause some mild muscle soreness when heading out on the ice.</h4>
                 <br>
-                <img src="./images/iceskating.jpg" alt= "Ice Skating" height=500px />
+                <img src="./images/iceskating.jpg" alt= "Ice Skating" height=500px style="width: 100%"/>
 
                 <h3>Go-Karting Trip</h3>
                 <br>
                 <h4>When school starts going by too fast, that means it is time to gear up for a race against fellow members on the track. A few members went down to Fastimes Indoor Karting, a track with performance go-karts with top speeds of 40 mph, in Indianapolis on April 1st, 2017. After a couple races, it was time for lunch at the nearby Steak 'n Shake.</h4>
                 <br>
-                <img src="./images/go-karting.jpg" alt= "Go-Karting Trip" height=500px />
+                <img src="./images/go-karting.jpg" alt= "Go-Karting Trip" height=500px style="width: 100%"/>
 
                 <h3>Ice Cream Social<h3>
                 <br>
@@ -78,7 +78,7 @@
                 <br>
                 <h4>IEEE hosted a final picnic with free food on April 21st, 2017 as a way to keep calm as Dead Week loomed in the week ahead.</h4>
                 <br>
-                <img src="./images/picnic.jpg" alt= "Spring Picnic" height=500px />
+                <img src="./images/picnic.jpg" alt= "Spring Picnic" height=500px style="width: 100%"/>
 
                 <h3>Movie Nights</h3>
                 <br>
