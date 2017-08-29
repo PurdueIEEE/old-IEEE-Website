bf3622845a1954bb37904dec0bfde7d70c246a45
Brandon Stewart <stewartb2@gmail.com>
2017-08-29 17:04:28 -0400

    Update Learning Page
    Need to add times and locations still

--- learning/index.php
+++ learning/index.php
@@ -11,43 +11,15 @@
                 <h1>IEEE Learning Workshops</h1>
 
                 <h1>Upcoming and In Progess Events:</h1>
-                <h2>CATIA Training Session</h2>
-                <p>This training session will provide a basic overview of CATIA, a CAD software often used in the automotive, aerospace, and other heavy industries. We will delve into the basics of the software, so that members can become familiar with the software and gain a greater understanding of CAD in general. No prior knowledge is necessary. A few take-home projects will be given.</p>
-                <ul>
-                    <li>Lesson 1: Tuesday, September 13th in SC G046 from 7:30 to 9:30 PM</li>
-                    <li>Lesson 2: Sunday, September 18th in SC G046 from 7:30 to 9:30 PM</li>
-                    <li>Lesson 3: Thursday, September 22nd in SC G046 from 7:30 to 9:30 PM</li>
-                </ul>
-                <p><strong>Contact for additional questions and help:</strong></p>
-                <ul>
-                    <li>Yash Nain: ynain@purdue.edu</li>
-                    <li>Michael Malachowski: mmalacho@purdue.edu</li>
-                    <li>Joey Williamson: will1361@purdue.edu</li>
-                    <li>Robert McIntyre: mcintyr2@purdue.edu</li>
-                    <li>Ajay Kumar: ajayk@purdue.edu</li>
-                </ul>
-                <p>Yash will remain for an extra hour to answer any questions, and will hold officer hours every Friday from 10:00 A.M. to 1:00 P.M. He will not be available for the last session.</p>
-
-                <h2>Electric Vehicle Technology</h2>
-                <p>This seminar will be focused on the various aspects of electric vehicle technology. Throughout the session, we will go over what goes in to modern day electric vehicles and how they relate to student projects. There will be an emphasis on the evGrandPrix Go-Karts that are built each year to race in the electric counterpart to the GrandPrix at the Indianapolis Motor Speedway.</p>
-                <ul>
-                    <li>Monday, September 26th in ME 1051 from 6:30 to 7:30 PM</li>
-                </ul>
-                <p><strong>Contact for additional questions and help:</strong></p>
-                <ul>
-                    <li>Steven Padilla: spadilla@purdue.edu</li>
-                </ul>
-                <p>Steven will be available for office hours for additional help Thursday 22, from 2:30 to 3:20 PM in EE 014</p>
-
-                <h1>Past Events:</h1>
+                <h2>3D Priting</h2>
+                <p>3D printing refers to the process of creating 3-dimensional objects from layers of material.This will be organized as a showcase with an introduction to 3D printer filaments, how to use slicers, and how to purchase your first printer. </p>
+                <h2>Soldering</h2>
+                <p>Soldering is joining of metals through melting one of them, or using a filler metal to join the metals together. Building a circuit board usually involves some form of soldering in order to join all supplemental components onto the board. This workshop will include the basics on how to correctly and safely solder boards in order to produce a functioning circuit. This is useful for those interested in electronics and in Senior Design.
+                </p>
 
                 <h2>EAGLE Training Session</h2>
-                <p>This training session will cover the basics of EAGLE, an electrical board schematic and layout tool. The training session is split into three parts, that will teach how to create schematics, board layouts, and parts. No prior knowledge is required. The first session will be held this Thursday, in Physics 203 at 6:30 PM. Additional details below:</p>
-                <ul>
-                    <li>Lesson 1: Thursday, September 8th in Phys 203 from 6:30 to 8:30 PM</li>
-                    <li>Lesson 2: Thursday, September 15th in Hamp 1252 from 6:30 to 8:30 PM</li>
-                    <li>Lesson 3: Thursday, September 22nd in Hamp 1144 from 6:30 to 8:30 PM</li>
-                </ul>
+                <p>This training session will cover the basics of EAGLE, an electrical board schematic and layout tool. The training session is split into three parts, that will teach how to create schematics, board layouts, and parts. No prior knowledge is required. Additional details below:</p>
+
                 <p><strong>TODO: Before you come to this session, there are a few requirements.</strong></p>
                 <ul>
                     <li>Download EAGLE and the Sparkfun Libraries. Instructions on downloading and setup can be found on <a href="https://learn.sparkfun.com/tutorials/how-to-install-and-setup-eagle">Sparkfun's website.</a></li>
@@ -57,70 +29,22 @@
                 <ul>
                     <li>Laptop with EAGLE installed, a mouse (recommended), charged battery (some outlets available)</li>
                 </ul>
-                <p><strong>Contact for additional questions and help:</strong></p>
-                <p>Officer Hours: After each lesson, there will be open office hours for questions and help on the past lesson. Luke will be holding office hours the following Tuesday and Thursday from 10:30 to 11:30 AM in the IEEE Office (EE 014).</p>
-                <ul>
-                    <li>Luke McBee: lmcbee@purdue.edu</li>
-                    <li>Carolyn Lewlling: clewelli@purdue.edu</li>
-                    <li>Rodolfo Leiva: rleiva@purdue.edu</li>
-                </ul>
-
-                <h2>Introduction to Microcontrollers</h2>
-                <p>This training session will go over the basics of microcontrollers. It will go into the basics of writing code to work for Arduinos, what you can and can't do with an Arduino, and how to set up different hardware to connect devices to your Arduino. If you have any question about this workshop, feel free to contact me or the people below:</p>
-                <ul>
-                    <li>Lesson 1: Wednesday, September 14th in Hamp 1252 from 6:30 to 8:30 PM</li>
-                    <li>Lesson 2: Wednesday, September 21st in Arms 1109 from 6:30 to 8:30 PM</li>
-                </ul>
-                <p><strong>Contact for additional questions and help:</strong></p>
-                <ul>
-                    <li>Matt Molo: mmolo@purdue.edu</li>
-                    <li>Ryan Mcbee: rmcbee@purdue.edu</li>
-                    <li>Aditya Vaidyam: avaidyam@purdue.edu</li>
-                </ul>
 
                 <h2>SolidWorks Training Session</h2>
-                <p>Learn the basics of 3D modeling with one of the most widely utilized software’s in industry, SolidWorks! This introductory series will cover basic parametric modeling with a focus on best practices so that you can create better, more robust models. The training session is split into three parts, with the first session being held this Wednesday, in Physics 022 at 6:30 PM. Additional details below:</p>
-                <ul>
-                    <li>Lesson 1: Wednesday, September 7th in Phys 022 from 6:30 to 8:30 PM</li>
-                    <li>Lesson 2: Wednesday, September 14th in Phys 022 from 6:30 to 8:30 PM</li>
-                    <li>Lesson 3 (Two dates available, only need to attend 1 day):</li>
-                    <ul>
-                        <li>Wednesday, September 21st in Phys 022 from 6:30 to 8:30 PM</li>
-                        <li>Thursday, September 22nd in Phys 026 from 6:30 to 8:30 PM</li>
-                </ul>
-                </ul>
-                <p><strong>Contact for additional questions and help:</strong></p>
-                <p>Officer Hours: After each lesson, there will be open office hours for questions and help on the past lesson.</p>
-                <ul>
-                    <li>Joshua Berg: berg30@purdue.edu (1:30 - 2:20 PM MWF) EE 014</li>
-                    <li>Sanay Shah: shah196@purdue.edu (10:30 - 11:30 AM MF) EE 014</li>
-                    <li>Alex Ruffino: aruffino@purdue.edu (11:30 - 12:30 PM F) EE 014</li>
-                    <li>Sophia Duan: duan35@purdue.edu (12:30 - 1:20 PM TR) EE 014</li>
-                    <li>Joe Pejril: jpejril@purdue.edu (10:30 - 11:20 AM TR) EE 014</li>
-                </ul>
+                <p>Learn the basics of 3D modeling with one of the most widely utilized software’s in industry, SolidWorks! This introductory series will cover basic parametric modeling with a focus on best practices so that you can create better, more robust models.</p>
+
 
                 <h2>Git + GitHub</h2>
                 <p>This session will cover the basics of using git and GitHub. Git is a version control software that makes saving your old code and reverting changes much easier. GitHub is a remote location to store your code and collaborate with other people through merging and code reviews. The session will cover only command line tools, so familiarity with the command line is helpful.</p>
-                <ul>
-                    <li>Friday, September 30th in Phys 223 from 6:00 to 8:00 PM</li>
-                </ul>
-                <p><strong>Contact for additional questions and help:</strong></p>
-                <ul>
-                    <li>Brandon Stewart: stewa174@purdue.edu</li>
-                    <li>Aditya Vaidyam: avaidyam@purdue.edu</li>
-                </ul>
 
-                <h2>Introduction to Planes</h2>
-                <p>This training session will introduce people to the basics of aerodynamics, common terminology, and basic configurations of planes and their pros and cons. In addition, a brief overview of RC systems is given. We will use the Aerial Robotic's kit planes and custom plane as reference. It will be an open session, so anyone is welcome to come. No prior knowledge is required.</p>
-                <ul>
-                    <li>Saturday, September 10th in ARMS 1021 from 1:30 to 3:30 PM (Additional time is allotted for questions)</li>
-                </ul>
-                <p><strong>Contact for additional questions and help:</strong></p>
-                <ul>
-                    <li>Yash Nain: ynain@purdue.edu</li>
-                    <li>Michael Malachowski: mmalacho@purdue.edu</li>
-                    <li>Joey Williamson: will1361@purdue.edu</li>
-                </ul>
+                <h2>Arduino</h2>
+                <p>This training session will go over the basics of microcontrollers. It will go into the basics of writing code to work for Arduinos, what you can and can't do with an Arduino, and how to set up different hardware to connect devices to your Arduino. It is required that attendees have a competent understanding of reading and writing code (for example, students that take CS 159 or some equivalent)
+                </p>
+
+                <h2>Catia</h2>
+                <p>This training session will provide a basic overview of CATIA, a CAD software often used in the automotive, aerospace, and other heavy industries. We will delve into the basics of the software, so that members can become familiar with the software and gain a greater understanding of CAD in general. No prior knowledge is necessary.
+                </p>
+
 
             </div>
         </div>
