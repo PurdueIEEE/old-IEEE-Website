1a05e9423d9c8d76265808b1589d12e44ca83296
Brandon Stewart <stewartb2@gmail.com>
2017-11-17 22:22:27 -0500

    Added Matlab Workshop (#112)

---
+++ learning/matlab/index.php
@@ -0,0 +1,24 @@
+<?php 
+    $page_title = 'Learning Committee - MATLAB Workshops';
+    $site_title = 'Purdue IEEE';
+    $site_index = '/';
+    include '../../header.php';
+?>
+
+    <div class="well card-1">
+        <div class="row">
+            <div class="col-md-8 text-dark col-md-offset-2 text-dark">
+                <h1>MATLAB Workshops from IEEE Learning Committee</h1>
+
+                <p>The following files are used for the Introducation to MATLAB Workshop:</p>
+                <ul>
+                    <li><a href="./introduction_workshop.pdf">PDF of instructions and explanations"</a></li>
+                    <li><a href="./water_cooling.xls">Excel workbook of water cooling experiment data</a></li>
+                    <li><a href="./finished_files.zip">ZIP archive of completed workshop files for checking work</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+
+<?php include '../../footer.php'; ?>
