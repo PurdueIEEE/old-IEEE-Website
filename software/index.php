936811f976bf6b8384a74a1fb8027339d40bf187
et0s <etas@purdue.edu>
2018-01-11 08:01:14 -0500

    Adding index.php header, body, and footer

---
+++ software/index.php
@@ -0,0 +1,52 @@
+<?php
+    $site_title = 'IEEE Software Saturdays';
+    $site_index = '/softsat';
+    include '../header.php';
+?>
+<style>
+    #centerside{
+        width: 100%;
+    }
+</style>
+<!-- Content Row -->
+<div class="well card-1">
+    <div class="row">
+
+        <!-- Content Column -->
+        <div class="col-md-10 text-dark col-md-offset-1">
+            <h2 class="text-center">Software Saturdays</h2>
+            <br>
+            <hr>
+
+            <p class="text-justify">
+                Software Saturdays is the brand new event that's the offspring of Code Café, an event that has been very popular in the past (and we hope will continue to be). Software Saturdays would like to expand on that idea, fostering the learning of coding and languages, but with a twist. Namely, we want to give the students a more rounded learning experience.
+            </p>
+
+            <p class="text-justify">
+                This required an expansion on the idea of Code Café. Rather than one Saturday, the track is comprised of 5 to 6 Saturdays. The first 4 to 5 sessions will be 3 hours long, and for learning the parts that will be necessary for the project. The last day will be the project day, and the students will have 6 hours to work on the project. The sessions will be held bi-weekly to allow the students to work on their components in between the sessions as they wish. Each off week, there will be office hours held in case students come across questions, or want to work a little more on what they've learned so far.
+            </p>
+
+            <p class="text-justify">
+                The plan is to introduce projects and concepts that are useful in the world of software development. The projects that will be pursued during each semester will be picked based on what would be useful in engineering contexts, but that doesn't mean they're not useful beyond the bounds of engineering. Regardless of what major our students are pursuing, we aim to teach them a skill that will be usefull no matter what job or hobbies they pursue in their future.
+            </p>
+
+            <br>
+        </div>
+    </div>
+</div>
+<div class="well card-1 text-dark">
+        <div class="row text-center">
+            <h1 id="leaders">Leadership</h1>
+            <hr/>
+            <div class="row">
+                <!--Raghav Malik-->
+                <div class="col-md-4" id="centerside">
+                    <img src="images/Ian_Sibley.jpg" alt="Ian Sibley" class="img-circle" width=200px height=200px/>
+                    <h4>Ian Sibley</h4>
+                    <p>Chair<br/><br/></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include '../footer.php';?>
