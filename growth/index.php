d6f06e51cb18d16361f06cf46ce86caa7d546490
et0s <etas@purdue.edu>
2018-01-16 21:50:53 -0500

    Adding the growth folder (G&E Webpage)

---
+++ growth/index.php
@@ -0,0 +1,57 @@
+<?php
+    $site_title = 'IEEE Software Saturdays';
+    $site_index = '/software';
+    include '../header.php';
+?>
+<style>
+    #centerside{
+        width: 100%;
+    }
+</style>
+<!-- Content Row -->
+<div class="well card-1">
+    <div class="row">
+        <!-- Content Column -->
+        <div class="col-md-10 text-dark col-md-offset-1">
+            <h2 class="text-center">Growth &amp Engagement</h2>
+            <br>
+            <p class="text-justify">
+                Looking to spread the word about IEEE? Join the G&ampE committee, where we plan, organize, and staff recruitment events and create growth opportunities. At our info session we'll brainstorm growth areas for IEEE and make plans to make IEEE the best it can be!            </p>
+            <hr>
+            <h3 class="text-center">Planner</h3>
+            <p class="text-justify">
+                Help us plan and organize advertising events, create advertisements, and generally spread the glory of the almighty IEEE            </p>
+            <hr>
+            <h3 class="text-center">Advertiser</h3>
+            <p class="text-justify">
+                Spread the glory of the almighty IEEE by staffing and assisting with advertising events such as Show&Tells, flyering, chalking, and other promotional activities.   </p>
+            <hr>
+            <div class="col-md-12">
+                <div class="col-md-4" style="float: left;">
+                    <h4 class="media-heading">Info Session</h4>
+                    <p class="text-justify">Wednesday, January 17th, 7pm, EE 226</p>
+                </div>
+                <div class="col-md-4" style="float: right;">
+                    <h4 class="media-heading">Regular Meetings</h4>
+                    <p>Thursdays, 7pm, EE 226</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="well card-1 text-dark">
+        <div class="row text-center">
+            <h1 id="leaders">Leadership</h1>
+            <hr/>
+            <div class="row">
+                <!--Ellie Topi-->
+                <div class="col-md-4" id="centerside">
+                    <img src="images/Ellie_Topi.jpg" alt="Ellie Topi" class="img-circle" width=200px height=200px/>
+                    <h4>Ellie Topi</h4>
+                    <p>Chair<br/><br/></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include '../footer.php';?>
