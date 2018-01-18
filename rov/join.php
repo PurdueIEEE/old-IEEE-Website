da7b9177e3087bdcb94085c034fdcaf22c8a1e67
Jessica Chin <chin21@purdue.edu>
2018-01-17 19:53:40 -0500

    fix link

--- rov/join.php
+++ rov/join.php
@@ -26,17 +26,17 @@
             <h2 class="text-center">Join ROV</h2>
             <p>
                 The ROV team brings together expertise in electronics, mechanics, and software in order to create an unrivled vehicle. We intend to finish first place in the world, and we are therefore looking for dedicated and talented members to help us achieve this goal! If you enjoy beign a part of a fast-paced team and learn quickly look no further. We are diving into the build season and could use more help - especially electrically!
-            </p>		
+            </p>
             <p>
                 Applications for the 2018 Spring season are now open and will be open until 1/20 at 11:59 pm. If you are unsure if you want to join yet, come to our first meeting on Wed, January 17th 6:30pm-8:00pm at WALC 1121
             </p>
             <p>
-                The application can be filled out below or in a seperate window <a href="https://docs.google.com/forms/d/1i7trdpKCT1x5mK7xmuNGjoVLXBCKrRo8YQvNfozbRZE/edit">here</a>.
+                The application can be filled out below or in a seperate window <a href="https://docs.google.com/forms/d/e/1FAIpQLSdn2f_D0SQpJ3Vr_9dC7wwLUxzM05HBdZuMwtcun27pjzjVqQ/viewform">here</a>.
             </p>
             <p>
-                <iframe src="https://docs.google.com/forms/d/1i7trdpKCT1x5mK7xmuNGjoVLXBCKrRo8YQvNfozbRZE/edit" width="700" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdn2f_D0SQpJ3Vr_9dC7wwLUxzM05HBdZuMwtcun27pjzjVqQ/viewform" width="700" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
             </p>
-           
+
 
         </div>
 
