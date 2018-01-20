6ec00e590ec3d8eee0eb6c1d4d9ff2f8d2eabe30
et0s <etas@purdue.edu>
2018-01-20 16:26:54 -0500

    added software saturdays to calendar

--- calendar.php
+++ calendar.php
@@ -44,8 +44,7 @@
                         array('color' => 'AB8B00', 'src' => 'ce7l4e2pdhq23tqf3353pfmfp8@group.calendar.google.com'), #ROV 2015-2016
                         array('color' => 'AB8B00', 'src' => '87uv0bmrpiok3ghluf0v4fs5ag@group.calendar.google.com'), #ROV 2016-2017
                         array('color' => 'AB8B00', 'src' => 'iivv0f0bo0lvr1c42tn42g8cck@group.calendar.google.com'), #ROV 2017-2018
-                        array('color' =? '333333', 'src' =>
-'d606i7l0ahpasvr7gl3abatfi0@group.calendar.google.com' #Software Saturdays 2018
+                        array('color' =? '333333', 'src' => 'd606i7l0ahpasvr7gl3abatfi0@group.calendar.google.com' #Software Saturdays 2018
                     );
                     $list = "";
                     foreach ($calendar_sources as $cal) {
