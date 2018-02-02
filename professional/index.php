9a4f755ad7629ed10528ca2fcb12f1d23790d752
et0s <etas@purdue.edu>
2018-02-02 14:46:55 -0500

    Added index.php to professional (IR)

--- professional/index.php
+++ professional/index.php
@@ -1,19 +1,71 @@
 <?php 
-    $page_title = 'Industrial Relations';
+    $page_title = 'Industrial Relations Committee';
     $site_title = 'Purdue IEEE';
     $site_index = '/';
     include '../header.php';
 ?>
 
-    <div class="well card-1">
-        <div class="row">
-            <div class="col-md-8 text-dark col-md-offset-2 text-dark">
-                <h1>IEEE Industrial Relations</h1>
+<div class="well card-1">
 
-                <p>Get involved and meet companies in IEEE's Industrial Relations committee!</p>
+        <div class="row">
+
+            <div class="col-md-8 text-dark col-md-offset-2 text-dark">
+
+                <h1>IEEE Industrial Relations Committee</h1>
+                <p>The Industrial Relations Committee is in charge of organizing and planning Purdue IEEE’s corporate outreach and professional development programs. Events run by the Industrial Relations Committee include company workshops, training sessions, and recruiting events. The Industrial Relations Committee is also responsible for general Purdue IEEE fundraising activities, as well as assisting technical committee fundraising (from first contact to sponsor follow-up).
+                </p>
 
             </div>
+
         </div>
+
     </div>
+    <div class="well card-1">
+
+        <div class="row">
+
+            <div class="col-md-8 text-dark col-md-offset-2 text-dark">
+                
+                <h2>Previous Events</h2>
+                <hr><br>
+
+                <h3>LEED GREEN Workshop</h3>
+                <img src="images/LGW.png" alt="LEED Green Workshop" height="405" width="720">
+
+            </div>
+
+        </div>
+
+        <br>
+
+        <div class="row">
+
+            <div class="col-md-8 text-dark col-md-offset-2 text-dark">
+
+                <h3>Cypress PSoC-BLE Tech Talk</h3>
+                <img src="images/PSoC.png" alt="Cypress PSoC-BLE tech talk" height="405" width="720">
+
+            </div>
+
+        </div>
+
+        <br>
+
+        <div class="row">
+
+            <div class="col-md-8 text-dark col-md-offset-2 text-dark">
+
+                <h3>Industrial-Roundtable Prep Night</h3>
+                <img src="images/IRPrep.png" alt="Cypress PSoC-BLE tech talk" height="540" width="720">
+
+            </div>
+
+        </div>
+
+        <br><br><br>
+
+    </div>
+    
+</div>
 
 <?php include '../footer.php'; ?>
