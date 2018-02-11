a8af0d1f3b1836708046261ce8902490570a19ea
Efe Tas <etas@purdue.edu>
2018-02-10 21:48:08 -0500

    adding images and img links

--- events.php
+++ events.php
@@ -23,6 +23,7 @@
             <img class = "img-lectures" src="images/eIISE.png" alt="IISE" height="150" width="215">
             <img class = "img-lectures" src="images/eSX.png" alt="Sigma Chi" height="150" width="215">
             <img class = "img-lectures" src="images/eATK.png" alt="Orbital" height="150" width="215">
+            <img class = "img-lectures" src="images/ePESC.png" alt="PESC" height="150" width="215">
 
             <br><br>
             <div class="col-md-8 text-dark col-md-offset-2 text-dark">
