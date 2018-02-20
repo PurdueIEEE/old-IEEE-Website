ad565229145a91a6ad99b644d6a07db7712753f4
et0s <etas@purdue.edu>
2018-02-19 19:19:32 -0500

    Reverting Website

--- index.php
+++ index.php
@@ -5,20 +5,20 @@
     $carousel = true; //Carousel Plugin (Slideshow) set true 
     include 'header.php'; //Include keyword, preprocesses the header.php file
 ?>
-    <!-- Lecture Slide Streaming (Replaces Carousel)-->
+    <!-- Lecture Slide Streaming (Replaces Carousel)
     <div class="row">
         <div class="col-lg-12">
             <header id="myCarousel" class="carousel slide card-2">
             <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fpurdueieee%2Fvideos%2F1694568840604678%2F&show_text=0&width=1140" width="1140" height="960" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
             </header>
         </div>
-    </div>
+    </div>-->
    
-    <!--<div class="row">
+    <div class="row">
         <div class="col-lg-12">
-            <!-- Header Carousel
+            <!-- Header Carousel -->
             <header id="myCarousel" class="carousel slide card-2">
-                <!-- Wrapper for slides
+                <!-- Wrapper for slides -->
                 <div class="carousel-inner">
                     <div class="item active">
                         <div class="fill" style="background-image:url('/images/ieee.jpg');"></div>
@@ -70,7 +70,7 @@
                     </div>
                 </div>
 
-                <!-- Controls
+                <!-- Controls-->
                 <a class="left carousel-control" href="#myCarousel" data-slide="prev">
                     <span class="icon-prev"></span>
                 </a>
@@ -79,9 +79,9 @@
                 </a>
             </header>
         </div>
-    </div>-->
+    </div>
 
-    <br><br><br><br><br><br><br>
+    <br><br><!--<br><br><br><br><br>-->
     <!-- Upcoming Events List -->
     <div class="well card-1 text-dark">
         <div class="row">
