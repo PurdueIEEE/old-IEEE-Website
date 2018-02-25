aa874aee38263b16b92d67081647f8835a7523b7
Michael Hayashi <michaelsedge@comcast.net>
2018-02-25 12:40:29 -0500

    More on Basic MATLAB files (#119)

--- learning/matlab/index.php
+++ learning/matlab/index.php
@@ -10,11 +10,18 @@
             <div class="col-md-8 text-dark col-md-offset-2 text-dark">
                 <h1>MATLAB Workshops from IEEE Learning Committee</h1>
 
-                <p>The following files are used for the Introducation to MATLAB Workshop:</p>
+                <h2>Files for Introducation to MATLAB Workshop</h2>
                 <ul>
-                    <li><a href="./introduction_workshop.pdf">PDF of instructions and explanations"</a></li>
+                    <li><a href="./introduction_workshop.pdf">PDF of complete instructions and explanations</a></li>
                     <li><a href="./water_cooling.xls">Excel workbook of water cooling experiment data</a></li>
-                    <li><a href="./finished_files.zip">ZIP archive of completed workshop files for checking work</a></li>
+                    <li><a href="./finished_workshop1.zip">ZIP archive of completed workshop files for checking work</a></li>
+                </ul>
+
+                <h2>Files for More on Basic MATLAB Workshop</h2>
+                <ul>
+                    <li><a href="./more_basics_workshop.pdf">PDF of complete instructions and explanations</a></li>
+                    <li><a href="./IEEE_officers.csv">CSV file with table of 2017-2018 IEEE Executive Committee</a></li>
+                    <li><a href="./finished_workshop2.zip">ZIP archive of completed workshop files for checking work</a></li>
                 </ul>
 
             </div>
