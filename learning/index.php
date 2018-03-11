a63458fad704306fa013f79ae082049d6968d233
et0s <etas@purdue.edu>
2018-03-11 00:08:52 -0500

    Updating learning

--- learning/index.php
+++ learning/index.php
@@ -8,17 +8,74 @@
     <div class="well card-1">
         <div class="row">
             <div class="col-md-8 text-dark col-md-offset-2 text-dark">
-                <h1>IEEE Learning Workshops</h1>
+                <h1>IEEE Learning</h1>
+				<p>We have regular planning meetings in ME 2004 on Tuesdays at 6:30PM. Come to these meetings if you want to get involved in IEEE by helping plan and organize learning events! Please contact the current chair, Justin Joco, at jjoco@purdue.edu or visit the IEEE office in EE 014 if you have any questions or concerns.
+				</p>
+				<hr>
+                <h2>Upcoming in Spring 2018</h2>
+				
+				<hr>
+				<img src = "images/Soldering.jpg" style="width: 100%">
+                <h3>IEEE x SWE x WiA Soldering Workshops</h3>
+					<p>Come to our soldering workshop sessions on Thurs, March 22nd or on Thurs, March 29th at 6PM in FRNY G124 to learn how to solder! This is brought to you by Purdue IEEE, Purdue Society of Women of Engineers (PSWE), and Women in Aerospace(WiA). We will
+					be soldering a 4-bit binary adder to learn both surface-mount
+					soldering and through-hole soldering. Please RSVP <a href="https://goo.gl/forms/CfUEdsZRWmjyrb4O2" style="text-decoration:underline" target="_blank">HERE</a>.
+					IMPORTANT: Members of the above clubs participate for free! All others have to pay $5 to participate.</p>
+				<hr>
+				<img src = "images/codecafe_final_w_date.jpg" style="width: 100%">
+                <h3>Code Cafe</h3>
+					<p>Code Cafe is back this spring on Saturday, March 31st from 1PM - 6PM in WALC B074! Learn the basics of programming using Python, one of the most intuitive and popular coding languages, and enjoy a free coffee/expresso bar with snacks! Please bring your laptop and charger. To RSVP as a student, fill out this form. Spots fill up quickly! However, if you're good at Python and are interested to be a mentor (accepted mentors get a free T-shirt), please apply here.
+					Visit our website at <a href="https://codecafe.purdueieee.org" style="text-decoration:underline" target="_blank">Code Cafe</a></p>
+				
+				<hr>
+			
+                <h2>Completed in Spring 2018</h2>
 
-                <h1>Upcoming and In Progess Events:</h1>
-                <h2>3D Printing</h2>
-                <p>3D printing refers to the process of creating 3-dimensional objects from layers of material.This will be organized as a showcase with an introduction to 3D printer filaments, how to use slicers, and how to purchase your first printer. </p>
-                <h2>Soldering</h2>
-                <p>Soldering is joining of metals through melting one of them, or using a filler metal to join the metals together. Building a circuit board usually involves some form of soldering in order to join all supplemental components onto the board. This workshop will include the basics on how to correctly and safely solder boards in order to produce a functioning circuit. This is useful for those interested in electronics and in Senior Design.
-                </p>
+				<hr>
+				<img src = "images/matlablogo.jpg" style="width: 100%">
+				<h3>Intro to MATLAB</h3>
+					<p>If you want to learn the basics of MATLAB or you want to review for ENGR 132, come to our MATLAB workshop in SC 179 on Saturday, February 3rd at 1PM! MATLAB is a popular numerical computing environment used to implement algorithms, simulate experiments, create user interfaces, and more. We’ll be going over its user interface, how to do math operations, save data, write functions, plotting, and other fundamentals.
+					</p>
+				<h3>Basics of MATLAB: Part 2</h3>
+					<p>If you want to dive deeper into the basics of MATLAB whether to build upon ideas from ENGR 132 or become more proficient using numerical computing for STEM work in college or afterwards, come to our next MATLAB workshop! This is a continuation of the Introduction to MATLAB Workshop that explores more about this a popular numerical computing environment.. NOTE: If this is your first MATLAB workshop, we recommend reviewing exercises from the previous workshop available here
+					</p>
 
-                <h2>EAGLE Training Session</h2>
-                <p>This training session will cover the basics of EAGLE, an electrical board schematic and layout tool. The training session is split into three parts, that will teach how to create schematics, board layouts, and parts. No prior knowledge is required. Additional details below:</p>
+				<hr>
+				<img src = "images/prosthetic.jpg" style="width: 100%">
+                <h3>Prosthetics Workshop</h3>
+					<p>Purdue IEEE Aerial Robotics and EMBS collaborated to host a composites workshop focused on the engineering of prosthetics. The event happened on January 27th and last from 1pm to 4pm. It began with a keynote speaker who introduced the topic of prosthetics design, and the rest of the day was dedicated to the building of the prosthetics.
+					</p>
+
+				<hr>
+				<img src = "images/career-fair.jpg" style="width: 100%">
+                <h3>"How to Career Fair" Prep Session</h3>
+                    <p>Worried about the upcoming Spring Expo (Mon, Jan 30th) and other career fairs? Come to Purdue IEEE's prep session on Tuesday, January 23rd at 6PM in EE 117 to learn from experienced members about how to prepare for career fairs and how to get interviews! Come with questions for the Q&ampA at the end!
+					</p>
+				<hr>
+                <h2>Completed in Fall 2017</h2>
+				<hr>
+				<img src = "images/CodeCafeLivePic.JPG" style="width: 100%">
+                <h3>Code Cafe</h3>
+					<p>Ever wanted to learn how to code? Come to Code Cafe in WALC B074 from 1PM to 6PM on Saturday, October 14th to learn the basics of programming using Python, one of the most intuitive and popular coding languages. We have a free coffee/expresso bar and snacks! Please bring your laptop and charger! 
+					</p>
+				
+				<hr>
+				<img src = "images/3dPrint.png" style="width: 100%">
+                <h3>3D Printing</h3>
+					<p>3D printing refers to the process of creating 3-dimensional objects from layers of material.This will be organized as a showcase with an introduction to 3D printer filaments, how to use slicers, and how to purchase your first printer. 
+					</p>
+				
+				<hr>
+				<img src = "images/Soldering.jpg" style="width: 100%">
+				<h3>Soldering</h3>
+					<p>Soldering is joining of metals through melting one of them, or using a filler metal to join the metals together. Building a circuit board usually involves some form of soldering in order to join all supplemental components onto the board. This workshop will include the basics on how to correctly and safely solder boards in order to produce a functioning circuit. This is useful for those interested in electronics and in Senior Design.
+					</p>
+
+				<hr>
+                <img src = "images/eagle.png">
+                <h3>EAGLE Training Session</h3>
+					<p>This training session will cover the basics of EAGLE, an electrical board schematic and layout tool. The training session is split into three parts, that will teach how to create schematics, board layouts, and parts. No prior knowledge is required. Additional details below:
+					</p>
 
                 <p><strong>TODO: Before you come to this session, there are a few requirements.</strong></p>
                 <ul>
@@ -29,22 +86,47 @@
                 <ul>
                     <li>Laptop with EAGLE installed, a mouse (recommended), charged battery (some outlets available)</li>
                 </ul>
+				
+				<hr>
+				<img src = "images/solidworks.png" style="width: 100%">
+                <h3>SolidWorks Training Session</h3>
+					<p>Learn the basics of 3D modeling with one of the most widely utilized software’s in industry, SolidWorks! This introductory series will cover basic parametric modeling with a focus on best practices so that you can create better, more robust models.
+					</p>
 
-                <h2>SolidWorks Training Session</h2>
-                <p>Learn the basics of 3D modeling with one of the most widely utilized software’s in industry, SolidWorks! This introductory series will cover basic parametric modeling with a focus on best practices so that you can create better, more robust models.</p>
+				<hr>
+				<img src = "images/github.png" style="width: 100%">
+                <h3>Git + GitHub</h3>
+					<p>This session will cover the basics of using git and GitHub. Git is a version control software that makes saving your old code and reverting changes much easier. GitHub is a remote location to store your code and collaborate with other people through merging and code reviews. The session will cover only command line tools, so familiarity with the command line is helpful.
+					</p>
 
+				<hr>
+				<img src = "images/EmailLogo.jpg" style="width: 100%">
+				<h3>Professional Emails</h3>
+					<p>Want to impress or gain attention of companies with your writing skills? Often, you have to write emails to do so. It's easy to write an email, but it's a little more difficult to write professional ones. Come to this workshop in WALC 2088 at 6:30PM on Thursday, November 2nd to learn about the email etiquette and mechanics that'll get employers to treat you more seriously!
+					</p>
 
-                <h2>Git + GitHub</h2>
-                <p>This session will cover the basics of using git and GitHub. Git is a version control software that makes saving your old code and reverting changes much easier. GitHub is a remote location to store your code and collaborate with other people through merging and code reviews. The session will cover only command line tools, so familiarity with the command line is helpful.</p>
+				<hr>
+				<img src = "images/Arduino.png" style="width: 100%">
+                <h3>Arduino</h3>
+					<p>This training session will go over the basics of microcontrollers. It will go into the basics of writing code to work for Arduinos, what you can and can't do with an Arduino, and how to set up different hardware to connect devices to your Arduino. It is required that attendees have a competent understanding of reading and writing code (for example, students that take CS 159 or some equivalent)
+					</p>
 
-                <h2>Arduino</h2>
-                <p>This training session will go over the basics of microcontrollers. It will go into the basics of writing code to work for Arduinos, what you can and can't do with an Arduino, and how to set up different hardware to connect devices to your Arduino. It is required that attendees have a competent understanding of reading and writing code (for example, students that take CS 159 or some equivalent)
-                </p>
+				<hr>
+				<img src = "images/AshrafAlam.jpeg">
+                <h3>ECE Professor Talk - Dr. Ashraf Alam</h3>
+					<p>We will be hosting a professor talk with ECE Professor Muhammad Ashraful Alam, whose primary area of interest is in Microelectronics and Nano-techology. Please come to his talk, primarily focused on undergraduate students, on Tuesday, November 28th in ME 1009 at 6:30PM to learn more about “How a Synthesis of Biology and Electronics May Change the World as We Know It.” Free pizza and soda will be served!
+					</p>
+				
+				<hr>
+				<img src = "images/matlablogo.jpg" style="width: 100%">
+				<h3>MATLAB</h3>
+					<p>If you want to learn the basics of MATLAB or you want to get a headstart in ENGR 132, come to our MATLAB workshop in SC 179 on Saturday, November 18th at 1PM! MATLAB is popular numerical computing environment used to implement algorithms, simulate experiments, create user interfaces, and many more. We’ll be going over its user interface, how to do math operations, save data, write functions, plotting, and other fundamentals.
+					</p>
 
-                <h2>Catia</h2>
-                <p>This training session will provide a basic overview of CATIA, a CAD software often used in the automotive, aerospace, and other heavy industries. We will delve into the basics of the software, so that members can become familiar with the software and gain a greater understanding of CAD in general. No prior knowledge is necessary.
-                </p>
-
+				<hr>
+                <h3>Catia</h3>
+					<p>This training session will provide a basic overview of CATIA, a CAD software often used in the automotive, aerospace, and other heavy industries. We will delve into the basics of the software, so that members can become familiar with the software and gain a greater understanding of CAD in general. No prior knowledge is necessary.
+					</p>
 
             </div>
         </div>
