c8916f6a3bf6693405e3b367d6def95b879c97d2
Brandon Stewart <stewartb2@gmail.com>
2018-03-28 17:38:01 -0400

    Removed unused code from elections page

--- elections.php
+++ elections.php
@@ -8,7 +8,6 @@
     margin-left:100px;
 }
 </style>
-<!--<h1 style="margin-top: 0; text-align: center"><a href="https://docs.google.com/presentation/d/1G6h-emUWy3I2ls_OqpoE0mWVIHU4ZSFBLvp1TrGXsik/edit?usp=sharing" target="_blank">Callout Slides Link</a></h1>-->
 
 <div class="well card-1">
 
@@ -105,105 +104,4 @@
                    
                 </div>
         </div>
-        <!-- <div class="row">
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdnlhyjN7CslLFx-KYKXZ3EAnb-layl2ucR1yXe5JJj6Y9UVw/viewform?embedded=true" width="1130" height="600" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
-        </div> -->
 </div>
-<!-- Callout Information
-    <h1 style="margin-top: 0">Callout</h1>
-<p>We will go over the different technical committees that make up IEEE along with the professional, learning, and social aspects. <font size="+0.2"> There will be free pizza and drinks provided.</font></p>
--->
-<!-- First Callout 
-    <div id="leftside" style="float:left;">
-    <p><strong>Date:</strong> Tuesday 01/16</p>
-    <p><strong>Time:</strong> 6:00pm - 7:30pm</p>
-    <p><strong>Place:</strong> EE 129</p>
-</div> -->
-<!-- Second Callout
-    <div id="rightside" style="float:left;">
-    <p><strong>Date:</strong> Thursday 08/31</p>
-    <p><strong>Time:</strong> 6:30pm - 7:30pm</p>
-    <p><strong>Place:</strong> EE 129</p>
-</div> -->
-<br>
-<!--<h1 style="margin-top: 0">Committee Meetings</h1>
-<p>Additionally, if you are interested in joining a committee after seeing them at our callout, their first meetings and contact info are listed below:<p>
-
-<h4>Cornerstones Committees</h4>
-<div class="col-md-12">
-	<div class="col-md-4">
-        <h4 class="media-heading"><a href="/learning/">Learning</a></h4>
-        <p>Tues, January 16th 8:00pm-9:00pm at ME 2004</br>
-         Justin Joco (jjoco@purdue.edu)</p>
-    </div>
-
-    <div class="col-md-4">
-        <h4 class="media-heading"><a href="/professional/">Professional</a></h4>
-        <p>Tues, Jan 23rd 6:00pm–7:00pm at GRIS 118</br>
-        Reshef Elisha (relisha@purdue.edu)</p>
-</div>
-
-    <div class="col-md-4">
-        <h4 class="media-heading"><a href="/social/">Social</a></h4>
-        <p>Thurs, Jan 11th 6:30pm-7:15pm at PHYS 117</br>
-        Yash Nain (ynain@purdue.edu)</p>
-    </div>
-</div>
-<div class="col-md-12">
-    <div class="col-md-4">
-        <h4 class="media-heading">Growth and Engagement<!--<a href="//">Link Here</a></h4>
-        <p>Thurs, Jan 25th 7:00pm-7:30pm at TBD</br>
-        Ellie Topi (etopi@purdue.edu)</p>
-    </div>
-</div>
-
-<br><br><br>
-<h4>Technical Committees</h4>
-<div class="col-md-12">
-    <div class="col-md-4">
-        <h4 class="media-heading"><a href="/partieee/">Aerial Robotics</a></h4>
-        <p>Thurs, January 18th 6:30 pm-7:30pm at TBD</br>
-        Ellie Topi (etopi@purdue.edu)</p>
-    </div>
-
-    <div class="col-md-4">
-        <h4 class="media-heading"><a href="/csociety/">Computer Society</a></h4>
-        <p>Thurs, January 18th 6:30pm-7:30pm at EE 222</br>
-        Raghav Malik (malik22@purdue.edu)</p>
-    </div>
-
-    <div class="col-md-4">
-        <h4 class="media-heading"><a href="/embs/">Engineering in Medicine &amp Biology Society (EMBS)</a></h4>
-        <p>Wed, 17th January 7:30pm-8:30pm at HAMP 2101</br>
-        Apoorva Bhagwat (embs-info@purdueieee.org)</p>
-    </div>
-</div>
-<div class="col-md-12">
-    <div class="col-md-4">
-        <h4 class="media-heading"><a href="/mtts/">Microwave Theory & Techniques Society (MTT-S)</a></h4>
-        <p>Thurs, January 18th 6:00pm-7:00pm at EE 224</br>
-        Rayane Chatrieux (rchatrie@purdue.edu)</p>
-    </div>
-
-    <div class="col-md-4">
-        <h4 class="media-heading"><a href="/racing/">Racing</a></h4>
-        <p>Wed, January 17th 7:00pm-8:00pm at WALC 1132</br>
-        Adrian White (white378@purdue.edu)</br>
-    </div>
-
-    <div class="col-md-4">
-        <h4 class="media-heading"><a href="/rov/">Remotely Operated underwater Vehicle (ROV)</a></h4>
-        <p>Wed, January 17th 6:30pm-8:00pm at WALC 1121</br>
-        Alex Ruffino (aruffino@purdue.edu)</p>
-    </div>
-
-<br><br><br>
-</div>
-<h1 style="margin-top: 0">Software Saturdays</h1>
-<div class ="col-md-12">
-    <div class="col-md-4">
-        <h4 class="media-heading">Software Saturdays First Meeting<!--<a href="//">Link Here</a></h4>
-        <p>Sat, Feburary 10th 3:30pm-6:30pm at WALC 1087</br>
-        Ian Sibley (isibley@purdue.edu)</p>
-    </div>
-</div> -->
