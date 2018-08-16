cb0aa3b4d3fb68213d3a8540f44d53126598c172
Erik VanderWerf <vanderwe@purdue.edu>
2018-08-16 16:39:34 -0400

    Info sessions can link to a committee, which can be auto hyperlinked in PHP

--- info_sessions.php
+++ info_sessions.php
@@ -6,6 +6,7 @@
  * Time: 2:44 PM
  */
 
+include '_committees.php';
 
 $year = 2018;
 $etime = function ($month, $day, $hour, $minute=0) use ($year) {
@@ -13,25 +14,24 @@ $etime = function ($month, $day, $hour, $minute=0) use ($year) {
 };
 
 $info_sessions = array(
-    array($etime(8, 28, 18, 30), $etime(8, 28, 20, 0), 'EE 129', 'Purdue IEEE'),
-    array($etime(8, 29, 18, 30), $etime(8, 29, 20, 0), 'EE 129', 'Purdue IEEE'),
-    array($etime(8, 30, 18), $etime(8, 30, 19), 'EE 115', 'Racing'),
-    array($etime(8, 30, 18, 30), $etime(8, 30, 19, 30), 'ME 2061', 'Learning'),
-    array($etime(8, 30, 19), $etime(8, 30, 20), 'ARMS 3109', 'Social'),
-    array($etime(8, 30, 20), $etime(8, 30, 21), 'EE 115', 'Industrial Relations'),
-    array($etime(8, 31, 18), $etime(8, 31, 19), 'HEAV 206', 'EMBS'),
-    array($etime(9, 1, 11), $etime(9, 1, 12, 30), 'ME 1130', 'Software Saturdays'),
-    array($etime(9, 4, 18, 30), $etime(9, 4, 19, 30), 'ARMS 1010', 'Aerial Robotics'),
-    array($etime(9, 5, 18, 30), $etime(9, 5, 20), 'ME 1061', 'ROV'),
-    array($etime(9, 6, 18), $etime(9, 6, 19), 'EE 222', 'Growth & Engagement'),
-    array($etime(9, 6, 18), $etime(9, 6, 19), 'EE 224', 'MTT-S'),
-    array($etime(9, 8, 11), $etime(9, 8, 12, 30), 'ME 1130', 'Software Saturdays'),
-    array(null, null, null, 'Social'),
-    array(null, null, null, 'Computer Society'),
+    array($etime(8, 28, 18, 30), $etime(8, 28, 20, 0), 'EE 129', GENERAL),
+    array($etime(8, 29, 18, 30), $etime(8, 29, 20, 0), 'EE 129', GENERAL),
+    array($etime(8, 30, 18), $etime(8, 30, 19), 'EE 115', RACING),
+    array($etime(8, 30, 18, 30), $etime(8, 30, 19, 30), 'ME 2061', LEARNING),
+    array($etime(8, 30, 19), $etime(8, 30, 20), 'ARMS 3109', SOCIAL),
+    array($etime(8, 30, 20), $etime(8, 30, 21), 'EE 115', INDUSTRIAL_RELATIONS),
+    array($etime(8, 31, 18), $etime(8, 31, 19), 'HEAV 206', EMBS),
+    array($etime(9, 1, 11), $etime(9, 1, 12, 30), 'ME 1130', SOFTWARE_SATURDAYS),
+    array($etime(9, 4, 18, 30), $etime(9, 4, 19, 30), 'ARMS 1010', PARTIEEE),
+    array($etime(9, 5, 18, 30), $etime(9, 5, 20), 'ME 1061', ROV),
+    array($etime(9, 6, 18), $etime(9, 6, 19), 'EE 222', GROWTH),
+    array($etime(9, 6, 18), $etime(9, 6, 19), 'EE 224', MTTS),
+    array($etime(9, 8, 11), $etime(9, 8, 12, 30), 'ME 1130', SOFTWARE_SATURDAYS),
+    array(null, null, null, COMPUTER_SOCIETY),
 );
 
 $info_session_text = '';
-$last_day = null;
+$previous_day = null;
 
 foreach ($info_sessions as $session) {
     $start = $session[0]; $end = $session[1]; $place = $session[2]; $committee = $session[3];
@@ -42,17 +42,17 @@ foreach ($info_sessions as $session) {
         $day = '<b>Undetermined</b>';
         $time = '';
     }
-    if ($day == $last_day) {
+    if ($day == $previous_day) {
         $day = '';
     } else {
-        $last_day = $day;
+        $previous_day = $day;
     }
     $info_session_text .= '<div class="row">
 <div class="col-sm-3">
     <h4>'.$day.'</h4>
 </div>
 <div class="col-sm-9">
-    <div class="col-xs-4"><h4><b>'.$committee.'</b></h4></div>
+    <div class="col-xs-4"><h4>'.$committee.'</h4></div>
     <div class="col-xs-4"><h4>'.$time.'</h4></div>
     <div class="col-xs-4"><h4>'.$place.'</h4></div>
 </div>
