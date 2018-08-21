f85723e12048bbac7c9ab9ffb09b8ffe7c164e4b
Erik VanderWerf <eski787@gmail.com>
2018-08-21 11:25:17 -0400

    Updating SS survey links. (#132)

--- software/index.php
+++ software/index.php
@@ -19,8 +19,9 @@
                 <div class="col-md-12">
                     <div class="list-group row">
                       <h4>
-                        <a href="attendee.php" class="text-center list-group-item list-group-item-info col-xs-4 col-xs-offset-2">Join as an Attendee</a>
-                        <a href="mentor.php" class="text-center list-group-item list-group-item-info col-xs-4">Join as a Mentor</a>
+                          <a href="attendee.php" class="text-center list-group-item list-group-item-info col-xs-3 col-xs-offset-1">Join as an Attendee</a>
+                          <a href="mentor.php" class="text-center list-group-item list-group-item-info col-xs-3">Join as a Mentor</a>
+                          <a href="android.php" class="text-center list-group-item list-group-item-info col-xs-5">Join as an Android Assistant</a>
                       </h4>
                     </div>
                 </div>
@@ -29,18 +30,36 @@
             <hr>
 
             <p class="text-justify">
-            Software Saturdays is the brand new event that's the offspring of Code Café, which has been very popular in the past (and we hope will continue to be). Software Saturdays would like to expand on that idea, fostering the learning of coding and languages, but with a twist. Namely, we want to give the students a more rounded learning experience.            </p>
-
+                Software Saturdays is the brand new event that's the offspring of Code Café, which has been very popular
+                in the past (and we hope will continue to be). Software Saturdays would like to expand on that idea,
+                fostering the learning of coding and languages, but with a twist. Namely, we want to give the students a
+                more rounded learning experience.
+            </p>
             <p class="text-justify">
-            This required an expansion on the idea of Code Café. Rather than one Saturday, the track is comprised of 5 to 6 Saturdays. The first 4 to 5 sessions will be 3 hours long, and for learning the parts that will be necessary for the project. The last day will be the project day, and the students will have 6 hours to work on the project and tie all those pieces together. The sessions will be held bi-weekly to allow the students to work on their components in between the sessions, and each off week there will be office hours held in case students come across questions as well.            </p>
-
+                This required an expansion on the idea of Code Café. Rather than one Saturday, the track is comprised of
+                5 to 6 Saturdays. The first 4 to 5 sessions will be 3 hours long, and for learning the parts that will
+                be necessary for the project. The last day will be the project day, and the students will have 6 hours
+                to work on the project and tie all those pieces together. The sessions will be held bi-weekly to allow
+                the students to work on their components in between the sessions, and each off week there will be office
+                hours held in case students come across questions as well.
+            </p>
             <p class="text-justify">
-            The plan is to introduce projects and concepts that are useful in the world of software development. The projects that will be pursued during each semester will be picked based on what would be useful in engineering contexts, but that doesn't mean they're not useful beyond the bounds of engineering. Regardless of what major our students are pursuing, we aim to teach them a skill that will be usefull no matter what job or hobbies they pursue in their future.            </p>
-
+                The plan is to introduce projects and concepts that are useful in the world of software development. The
+                projects that will be pursued during each semester will be picked based on what would be useful in
+                engineering contexts, but that doesn't mean they're not useful beyond the bounds of engineering.
+                Regardless of what major our students are pursuing, we aim to teach them a skill that will be usefull no
+                matter what job or hobbies they pursue in their future.
+            </p>
             <p class="text-justify">
-            This semester's plan is to do a beginner's web development track, showing the basics of how a webpage is hosted by a server, and how information typically flows between the page, the server, and a database. We'll be using MySQL Workbench for the database, the Nodejs module Express for the server (and its mysql module to talk to the database), and a straightforward HTML, CSS, and JavaScript/jQuery webpage.
+                This semester's plan is to do a beginner's web development track, showing the basics of how a webpage is
+                hosted by a server, and how information typically flows between the page, the server, and a database.
+                We'll be using MySQL Workbench for the database, the Nodejs module Express for the server (and its mysql
+                module to talk to the database), and a straightforward HTML, CSS, and JavaScript/jQuery webpage.
+            </p>
             <p class="text-justify">
-            For further questions or interest, especially in attending or mentoring and teaching, feel free to email <b>software-saturdays@purdueieee.org</b> for more information.</p>
+            For further questions or interest, especially in attending or mentoring and teaching, feel free to email
+            <b>software-saturdays@purdueieee.org</b> for more information.
+            </p>
         </div>
     </div>
 </div>
