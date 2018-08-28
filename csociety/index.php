7dbe31fe55d3f76716d7cd47f5a9c2d96de2adc9
Brandon Stewart <stewartb2@gmail.com>
2018-08-27 21:46:23 -0400

    Update Computer Society leadership

--- csociety/index.php
+++ csociety/index.php
@@ -12,7 +12,7 @@
             <!-- Content Column -->
             <div class="col-md-8 text-dark col-md-offset-2 text-dark">
                 <h2>Computer Society</h2>
-                <p>We are a group of highly curious people with quirks in the science of computation. We do cool stuff with computers and try to spread the enlightenment with who-so-ever matches the level of curiosity of the group. 3D printing, web developing, cyber security, high performance computing, machine learning, graphics and everything you've heard of as tech-y is discussed, talked and made here, feel free to drop by. Contact <a href="mailto:malik22@purdue.edu">Raghav Malik</a> for more details.</p>
+                <p>We are a group of highly curious people with quirks in the science of computation. We do cool stuff with computers and try to spread the enlightenment with who-so-ever matches the level of curiosity of the group. 3D printing, web developing, cyber security, high performance computing, machine learning, graphics and everything you've heard of as tech-y is discussed, talked and made here, feel free to drop by. Contact <a href="mailto:bts@purdue.edu">Brandon Stewart</a> for more details.</p>
                 <a href="https://twitter.com/PurdueCSociety"><i class="fa fa-twitter-square fa-4x"></i></a>
                 <a href="/csociety/images/csociety.jpg"
                     <p class="text-center"><img src="/csociety/images/csociety.jpg" alt="2016 Team" style="width:100%"></p>
@@ -27,25 +27,35 @@
             <h1 id="leaders">Leadership</h1>
             <hr/>
             <div class="row">
-                <!--Raghav Malik-->
                 <div class="col-md-4">
-                    <img src="images/Raghav_Malik.jpg" alt="Raghav Malik" class="img-circle" width=200px height=200px/>
-                    <h4>Raghav Malik</h4>
+                    <h4>Brandon Stewart</h4>
                     <p>Chair<br/><br/></p>
                 </div>
 
-                <!--Brandon Stewart-->
                 <div class="col-md-4">
-                    <img src="images/Brandon_Stewart.jpg" alt="Brandon Stewart" class="img-circle" width=200px height=200px/>
-                    <h4>Brandon Stewart</h4>
+                    <h4>Christopher Potter</h4>
                     <p>Vice Chair<br/><br/></p>
                 </div>
 
-                <!--Shaurya Sinha-->
                 <div class="col-md-4">
-                    <img src="images/Shaurya_Sinha.png" alt="Shaurya_Sinha" class="img-circle" width=200px height=200px/>
-                    <h4>Shaurya Sinha</h4>
+                    <h4>Sidharth</h4>
                     <p>Sponsorship Coordinator<br/> <br/></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <h4>Jeremy Patterson</h4>
+                    <p>Learning Coordinator<br/><br/></p>
+                </div>
+
+                <div class="col-md-4">
+                    <h4>Jasmine Singh</h4>
+                    <p>Event Coordinator<br/><br/></p>
+                </div>
+
+                <div class="col-md-4">
+                    <h4>Joven Garces</h4>
+                    <p>Project Coordinator<br/> <br/></p>
                 </div>
             </div>
         </div>
