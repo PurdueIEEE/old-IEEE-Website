128830fabdb3b9366e68d595a7fcdaf5c984933b
isibley765 <isibley@purdue.edu>
2018-08-28 00:26:43 -0400

    Errors in the MTTS page, div tags left unclosed (cascaded inside of themselves) and Alden's picture href tag was all lowercase, unlike the filename in their images folder

--- mtts/index.php
+++ mtts/index.php
@@ -52,19 +52,22 @@
                     <div class="row">
                         <!--Alden Fisher-->
                         <div class= "col-md-4">
-                            <img src="images/alden_fisher.jpg" alt= "Alden Fisher" class="img-circle" width=250px height=250px />
+                            <img src="images/Alden_Fisher.JPG" alt= "Alden Fisher" class="img-circle" width=250px height=250px />
                             <h4>Alden Fisher</h4>
                             <p>Sponsorship Coordinator<br />Electrical Engineering<br />First-Year Master's Student<p>
+                        </div>
                         <!--Rayane Chatrieux-->
                         <div class= "col-md-4">
                             <img src="" alt= "Rayane Chatrieux" class="img-circle" width=250px height=250px />
                             <h4>Rayane Chatrieux</h4>
                             <p>Workshop Coordinator<br />Electrical Engineering<br />Senior</p>
+                        </div>
                         <!--Alan Han-->
                         <div class= "col-md-4">
                             <img src="images/alan_han.jpg" alt= "Alan Han" class="img-circle" width=250px height=250px />
                             <h4>Alan Han</h4>
                             <p>Event Coordinator<br />Electrical Engineering<br />Senior</p>
+                        </div>
                         </div>
                     </div>
 
