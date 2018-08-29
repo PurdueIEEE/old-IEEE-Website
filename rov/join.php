d75d46efbc1638f181f287ce2984aae7dc3f42cb
Brandon Stewart <stewartb2@gmail.com>
2018-08-28 21:31:30 -0400

    Add Callout Slides to website and update ROV page

--- rov/join.php
+++ rov/join.php
@@ -27,19 +27,14 @@
             <p>
                 The ROV team brings together expertise in electronics, mechanics, and software in order to create an unrivled vehicle. We strive to finish first place in the world, and we are therefore looking for dedicated and talented members to help us achieve this goal! If you enjoy beign a part of a fast-paced team and learn quickly look no further.
             </p>
-            <p>
-                Applications for the 2018 season are now closed. We will again open applictions at the beginning of the Fall 2018 semester.
-            </p>
             
-            <!--
             <p>
-                The application can be filled out below or in a seperate window <a href="https://docs.google.com/forms/d/e/1FAIpQLSdn2f_D0SQpJ3Vr_9dC7wwLUxzM05HBdZuMwtcun27pjzjVqQ/viewform">here</a>.
+                The application can be filled out below or in a seperate window <a href="https://goo.gl/forms/ZZligChCWP0X8gFW2">here</a>.
             </p>
             
             <p>
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdn2f_D0SQpJ3Vr_9dC7wwLUxzM05HBdZuMwtcun27pjzjVqQ/viewform" width="700" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+                <iframe src="https://goo.gl/forms/ZZligChCWP0X8gFW2" width="700" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
             </p>
-            -->
 
         </div>
 
