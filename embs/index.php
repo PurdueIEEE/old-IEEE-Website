dd21a56dbc30ae90fc3295f128c9ead1573a3450
Erik VanderWerf <eski787@gmail.com>
2018-09-03 12:03:39 -0400

    Embs update (#147)
    * Updating EMBS Officers
    
    * Cropping Dana image

--- embs/index.php
+++ embs/index.php
@@ -1,138 +1,151 @@
 <?php
-    $site_title = 'EMBS';
-    $site_index = '/embs';
-    include '../header.php';
+$site_title = 'EMBS';
+$site_index = '/embs';
+include '../header.php';
 ?>
 
-    <link href="/assets/bootstrap.embs.css" rel="stylesheet">
-    <!-- Content Row -->
-    <div class="well card-5" style="min-height: 490px; background-image: url('./images/brain.jpg'); background-size: cover; background-position: top;">
-        <div class="row text-center">
-            <div class="col-md-12" style="min-height: 490px; color:white; position: relative; top: 50%; transform: translateY(40%);">
-                <div>
-                    <img src="./images/EMBSLogoWhite.png" width="80%" />
-                    <p style="font-weight: 700; transform: translateX(2%);">PURDUE UNIVERSITY</p>
-                </div>
+<link href="/assets/bootstrap.embs.css" rel="stylesheet">
+<!-- Content Row -->
+<div class="well card-5"
+     style="min-height: 490px; background-image: url('./images/brain.jpg'); background-size: cover; background-position: top;">
+    <div class="row text-center">
+        <div class="col-md-12"
+             style="min-height: 490px; color:white; position: relative; top: 50%; transform: translateY(40%);">
+            <div>
+                <img src="./images/EMBSLogoWhite.png" width="80%"/>
+                <p style="font-weight: 700; transform: translateX(2%);">PURDUE UNIVERSITY</p>
             </div>
         </div>
     </div>
-        <div class="well card-1 text-dark">
-            <div class="row text-center">
-                <h1 id="mission">Mission</h1>
-                <hr />
-                <p>Engineering in Medicine and Biology Society (EMBS) is an internationally recognized technical society under IEEE, servicing the largest community of biomedical engineers in the world. The mission of EMBS is to focus on the development and application of engineering concepts and methods to provide new solutions to medical and biological problems. Its members apply concepts and methods of physical engineering and life sciences to issues in the rapidly expanding fields of medicine and biology. Since 1952, international EMBS has incubated global leaders and problem solvers from a host of backgrounds capable of addressing modern challenges.
-                <br /><br />The Purdue student chapter was founded in September 2015 and is the youngest committee within Purdue IEEE. It consists of a group of dedicated students who share a common passion for transforming and revolutionizing future medicine and healthcare. This society exposes its members to the people, practices, information, and ideas necessary for success in biomedical and related careers. It regularly conducts social events, seminars and tours through assistance of faculty and industry supporters; befitting a technical society, Purdue IEEE EMBS members diligently contribute to research initiatives, participate in competitions, attend conferences, and organize technical workshops for the cultivation of shared knowledge.
-                </p>
-                <p><a href="mailto:embs-info@purdueieee.org">Contact us for more information.</a></p>
-            </div>
-        </div>
-        <div class="well card-1 text-dark">
-            <div class="row text-center">
-                <h1 id="officers">Officers</h1>
-                <hr />
-                <div class="row">
-                    <!--Apoorva Bhagwat-->
-                    <div class= "col-md-4">
-                        <img src="./images/apoorva.jpg" alt= "Hayley Chan" class="img-circle" width=250px height=250px />
-                        <h4>Apoorva Bhagwat </h4>
-                        <p>Chair<br />Biomedical Engineering with Certificate in Entrepreneurship<br />Junior</p>
-                    </div>
-                    <!--Eugene Kim-->
-                    <div class= "col-md-4">
-                        <img src="./images/white.jpg" alt= "Hayley Chan" class="img-circle" width=250px height=250px />
-                        <h4>Eugene Kim</h4>
-                        <p>Vice Chair<br />Biomedical Engineering<br />Junior</p>
-                    </div>
-                    <!--Kunal Sinha-->
-                    <div class= "col-md-4">
-                        <img src="./images/kunal.jpg" alt= "Hayley Chan" class="img-circle" width=250px height=250px />
-                        <h4>Kunal Sinha</h4>
-                        <p>Secretary<br />Computer Science<br />Sophmore</p>
-                    </div>
-                </div>
-                <br />
-                <div class="row">
-                    <!--Cyril Manjooran-->
-                    <div class= "col-md-4">
-                        <img src="./images/white.jpg" alt= "Cooper Pratt" class="img-circle" width=250px height=250px />
-                        <h4>Cyril Manjooran</h4>
-                        <p>Student Liaison<br />Industrial Engineering<br /></p>
-                    </div>
-                    <!--Dana Moryl-->
-                    <div class= "col-md-4">
-                        <img src="./images/white.jpg" alt= "Justin Joco" class="img-circle" width=250px height=250px />
-                        <h4>Dana Moryl</h4>
-                        <p>Faculty Liaison<br />Biomedical Engineering<br />Junior</p>
-                    </div>
-                    <!--Barnabas Obeng-Gyasi-->
-                    <div class= "col-md-4">
-                        <img src="./images/white.jpg" alt= "Celine Chang" class="img-circle" width=250px height=250px />
-                        <h4>Barnabas Obeng-Gyasi</h4>
-                        <p>Industry Liaison<br /> <br /> </p>
-                    </div>
-                </div>
-                <br /><br />
-            </div>
-        </div>
-        <div class="well card-1 text-dark">
-            <div class="row text-center">
-                <h1 id="events">Past Events</h1>
-                <hr />
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="./images/Sabbagh.gif" alt= "Placeholder" class="img-rounded" width=200px height=250px />
-                        <h4>Dr. Harold Sabbagh</h4>
-                        <p>Modeling neuronal functionality in PSPICE as an electrical circuit.</p>
-                    </div>
-                    <div class="col-md-6">
-                        <img src="./images/rocky.jpg" alt= "Placeholder" class="img-rounded" width=200px height=250px />
-                        <h4>Dr. Rocky Clancy</h4>
-                        <p>Engineering Ethics.</p>
-                    </div>
-                </div>
-                <br /><br />
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="./images/amy.jpg" alt= "Placeholder" class="img-rounded" width=220px height=250px />
-                        <h4>Dr. Amy Brewster</h4>
-                        <p>Neuroethics.</p>
-                    </div>
-                    <div class="col-md-6">
-                        <img src="./images/song.jpg" alt= "Placeholder" class="img-rounded" width=200px height=250px />
-                        <h4>Dr. Yujia Song</h4>
-                        <p>Biomedical Ethics.</p>
-                    </div>
-                </div>
-                <br /><br />
-            </div>
-        </div>
-        <div class="well card-1 text-dark">
-            <div class="row text-center">
-                <h1 id="projects">Projects</h1>
-                <hr />
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="./images/EEG.jpg" alt= "EEG" class="img-rounded" width=300px height=250px />
-                    </div>
-                    <div class="col-md-6">
-                        <br /><br />
-                        <h4>Electroencephalogram (EEG)</h4>
-                        <p>An EEG can be used to interpret the brain's neural activity at a low resolution for both clinical diagnostics and to develop assistive devices.</p>
-                    </div>
-                </div>
-                <br /><br />
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="./images/CoopWithDog.jpg" alt= "Zeus?" class="img-rounded" width=300px height=250px />
-                    </div>
-                    <div class="col-md-6">
-                        <br /><br />
-                        <h4>Dog Shelter</h4>
-                        <p>We're partnering with Circle-K on volunteering in the local community, building biomedical instruments and devices to aid veterinarians and dogs alike.</p>
-                    </div>
-                </div>
-            </div>
+</div>
+<div class="well card-1 text-dark">
+    <div class="row text-center">
+        <h1 id="mission">Mission</h1>
+        <hr/>
+        <p>Engineering in Medicine and Biology Society (EMBS) is an internationally recognized technical society under
+            IEEE, servicing the largest community of biomedical engineers in the world. The mission of EMBS is to focus
+            on the development and application of engineering concepts and methods to provide new solutions to medical
+            and biological problems. Its members apply concepts and methods of physical engineering and life sciences to
+            issues in the rapidly expanding fields of medicine and biology. Since 1952, international EMBS has incubated
+            global leaders and problem solvers from a host of backgrounds capable of addressing modern challenges.
+            <br/><br/>The Purdue student chapter was founded in September 2015 and is the youngest committee within
+            Purdue IEEE. It consists of a group of dedicated students who share a common passion for transforming and
+            revolutionizing future medicine and healthcare. This society exposes its members to the people, practices,
+            information, and ideas necessary for success in biomedical and related careers. It regularly conducts social
+            events, seminars and tours through assistance of faculty and industry supporters; befitting a technical
+            society, Purdue IEEE EMBS members diligently contribute to research initiatives, participate in
+            competitions, attend conferences, and organize technical workshops for the cultivation of shared knowledge.
+        </p>
+        <p><a href="mailto:embs-info@purdueieee.org">Contact us for more information.</a></p>
     </div>
-    <!-- /.well -->
+</div>
+<div class="well card-1 text-dark">
+    <div class="row text-center">
+        <h1 id="officers">Officers</h1>
+        <hr/>
+        <div class="row">
+            <!--Chair-->
+            <div class="col-md-4">
+                <img src="/images/officers/2018_Kim_Eugene.jpg" alt="Eugene Kim" class="img-circle"
+                     width=250px height=250px/>
+                <h4>Eugene Kim</h4>
+                <p>Chair<br/>Biomedical Engineering<br/>Senior</p>
+            </div>
+            <!--Vice-Chair-->
+            <div class="col-md-4">
+                <img src="./images/2018_Obeng-Gyasi_Barnabas.JPG" alt="Barnabas Obeng-Gyasi" class="img-circle"
+                     width=250px height=250px/>
+                <h4>Barnabas Obeng-Gyasi</h4>
+                <p>Vice Chair<br/>Neurobiology & Physiology<br/>Junior</p>
+            </div>
+            <!--Correspondence Liaison-->
+            <div class="col-md-4">
+                <img src="./images/2018_Moryl_Dana.jpg" alt="Dana Moryl" class="img-circle"
+                     width=250px height=250px/>
+                <h4>Dana Moryl</h4>
+                <p>Corespondance Liason<br/>Biomedical Engineering<br/>Senior</p>
+            </div>
+        </div>
+        <br/>
+        <div class="row">
+            <!--Industrial Liaison-->
+            <div class="col-md-6">
+                <img src="./images/2018_Bhagwat_Apoorva.jpg" alt="Apoorva Bhagwat" class="img-circle"
+                     width=250px height=250px/>
+                <h4>Apoorva Bhagwat</h4>
+                <p>Industrial Liaison<br/>Biomedical Engineering<br/>Senior</p>
+            </div>
+            <!--Technical Lead-->
+            <div class="col-md-6">
+                <img src="./images/white.jpg" alt="Sean Bucherl" class="img-circle" width=250px height=250px/>
+                <h4>Sean Bucherl</h4>
+                <p>Technical Lead<br/>Biomedical Engineering<br/>Senior</p>
+            </div>
+        </div>
+        <br/><br/>
+    </div>
+</div>
+<div class="well card-1 text-dark">
+    <div class="row text-center">
+        <h1 id="events">Past Events</h1>
+        <hr/>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="./images/Sabbagh.gif" alt="Placeholder" class="img-rounded" width=200px height=250px/>
+                <h4>Dr. Harold Sabbagh</h4>
+                <p>Modeling neuronal functionality in PSPICE as an electrical circuit.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="./images/rocky.jpg" alt="Placeholder" class="img-rounded" width=200px height=250px/>
+                <h4>Dr. Rocky Clancy</h4>
+                <p>Engineering Ethics.</p>
+            </div>
+        </div>
+        <br/><br/>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="./images/amy.jpg" alt="Placeholder" class="img-rounded" width=220px height=250px/>
+                <h4>Dr. Amy Brewster</h4>
+                <p>Neuroethics.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="./images/song.jpg" alt="Placeholder" class="img-rounded" width=200px height=250px/>
+                <h4>Dr. Yujia Song</h4>
+                <p>Biomedical Ethics.</p>
+            </div>
+        </div>
+        <br/><br/>
+    </div>
+</div>
+<div class="well card-1 text-dark">
+    <div class="row text-center">
+        <h1 id="projects">Projects</h1>
+        <hr/>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="./images/EEG.jpg" alt="EEG" class="img-rounded" width=300px height=250px/>
+            </div>
+            <div class="col-md-6">
+                <br/><br/>
+                <h4>Electroencephalogram (EEG)</h4>
+                <p>An EEG can be used to interpret the brain's neural activity at a low resolution for both clinical
+                    diagnostics and to develop assistive devices.</p>
+            </div>
+        </div>
+        <br/><br/>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="./images/CoopWithDog.jpg" alt="Zeus?" class="img-rounded" width=300px height=250px/>
+            </div>
+            <div class="col-md-6">
+                <br/><br/>
+                <h4>Dog Shelter</h4>
+                <p>We're partnering with Circle-K on volunteering in the local community, building biomedical
+                    instruments and devices to aid veterinarians and dogs alike.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /.well -->
 
 <?php include '../footer.php'; ?>
