f01e589ef67992c83341b5f50b25865b6b22cbe3
Erik VanderWerf <eski787@gmail.com>
2019-01-06 19:18:13 -0500

    Rov application (#163)
    * Reflowing ROV page.
    
    * Updating ROV join page.
    
    * Adding due date.

--- rov/index.php
+++ rov/index.php
@@ -1,7 +1,7 @@
 <?php
-    $site_title = 'IEEE ROV';
-    $site_index = '/rov';
-    include '../header.php';
+$site_title = 'IEEE ROV';
+$site_index = '/rov';
+include '../header.php';
 ?>
 
 <!-- Content Row -->
@@ -29,7 +29,9 @@
             <p><em>April 13, 2018 Update</em></p>
             <div>
                 <p>
-                The 2018 ROV Team pictures are now posted. See the complete team picture below and <a href=/rov/team/2018pictures.php>individual technical and administrative team pictures here</a>.
+                    The 2018 ROV Team pictures are now posted. See the complete team picture below and <a
+                            href=/rov/team/2018pictures.php>individual technical and administrative team pictures
+                        here</a>.
                 </p>
             </div>
             <hr>
@@ -39,40 +41,55 @@
 
             <h2 class="text-center">About The ROV Team</h2>
             <div class="col-md-6">
-                <img src="/rov/images/CephalopodByPool.jpg" alt="ROV Cephalopod by the pool" style="width:100%" style="float:left" style="display:block">
-                <p class="text-center">ROV <em>Cephalopod</em> </p>
+                <img src="/rov/images/CephalopodByPool.jpg" alt="ROV Cephalopod by the pool" style="width:100%"
+                     style="float:left" style="display:block">
+                <p class="text-center">ROV <em>Cephalopod</em></p>
             </div>
 
             <p class="text-justify">
-                The Remotely Operated underwater Vehicle (ROV) team was founded as a committee within Purdue IEEE Student Organization in 2008 with a small but dedicated group of five students. Today, the team's mission is to foster technical and professional skills of its members by designing, constructing, and testing an innovative underwater vehicle to compete in the MATE Center International ROV Competition.
+                The Remotely Operated underwater Vehicle (ROV) team was founded as a committee within Purdue IEEE
+                Student Organization in 2008 with a small but dedicated group of five students. Today, the team's
+                mission is to foster technical and professional skills of its members by designing, constructing, and
+                testing an innovative underwater vehicle to compete in the MATE Center International ROV Competition.
             </p>
 
             <p class="text-justify">
-                The ROV team is comprised of both engineering and non-engineering students working collaboratively on a hands-on project from the vehicle's design and prototyping phase, to its construction and testing, and ultimately competition performance. The team fosters a multidisciplinary spirit so that the best ideas may come forth. In addition to the mission, the MATE competition also requires a professional presentation, public display poster, and technical documentation.
+                The ROV team is comprised of both engineering and non-engineering students working collaboratively on a
+                hands-on project from the vehicle's design and prototyping phase, to its construction and testing, and
+                ultimately competition performance. The team fosters a multidisciplinary spirit so that the best ideas
+                may come forth. In addition to the mission, the MATE competition also requires a professional
+                presentation, public display poster, and technical documentation.
             </p>
 
             <p class="text-justify">
-                Because the vehicle requires excellence in design, construction, and presentation, the team members are themselves multidisciplinary collaborators and experts in their own discipline. Members learn to communicate clearly with each other and with clients while managing their own project responsibilities. Through various outreach events during the year, the team aims to inspire future generations to enter STEM (Science, Technology, Engineering, and Mathematics) fields.
+                Because the vehicle requires excellence in design, construction, and presentation, the team members are
+                themselves multidisciplinary collaborators and experts in their own discipline. Members learn to
+                communicate clearly with each other and with clients while managing their own project responsibilities.
+                Through various outreach events during the year, the team aims to inspire future generations to enter
+                STEM (Science, Technology, Engineering, and Mathematics) fields.
             </p>
 
             <p class="text-justify">
-                For more current and past information view the links to the left or contact <a href="mailto:rov@purdueieee.org">Alexander Ruffino</a>. The ROV team will be accepting new members at the beginning of the Fall 2018 semester. The current team picture is below.
+                For more current and past information view the links to the left or contact <a
+                        href="mailto:rov@purdueieee.org">Alexander Ruffino</a>. The ROV team will be accepting new
+                members at the beginning of the Fall 2018 semester. The current team picture is below.
             </p>
 
             <h2 class="text-center">2017-2018 Team Picture</h2>
             <a href="/rov/images/2018Full.jpg"
-                <p class="text-center"><img src="/rov/images/2018Full.jpg" alt="2018 Team" style="width:100%"></p>
+            <p class="text-center"><img src="/rov/images/2018Full.jpg" alt="2018 Team" style="width:100%"></p>
             </a>
             <br>
 
             <p><a href="/rov/team/2018pictures.php">See individual technical team pictures here</a></p>
             <br>
 
-            <p class="text-center"><a href="https://www.facebook.com/purdueieeerov"><i class="fa fa-facebook-square fa-4x"></i></a></p>
+            <p class="text-center"><a href="https://www.facebook.com/purdueieeerov"><i
+                            class="fa fa-facebook-square fa-4x"></i></a></p>
             <br>
         </div>
     </div>
 </div>
 <!-- /.well -->
 
-<?php include '../footer.php';?>
+<?php include '../footer.php'; ?>
