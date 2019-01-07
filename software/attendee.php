35e14f7233bab1957236c7a946cb4ae403c2d52e
isibley <isibley@purdue.edu>
2019-01-07 17:22:07 -0500

    Spring 2019 semester initial update

--- software/attendee.php
+++ software/attendee.php
@@ -14,17 +14,16 @@
         <!-- Sidebar Column -->
         <div class="col-md-3">
             <div class="list-group">
-                <a href="index.php" class="list-group-item">Software Saturdays</a>
+                <a href="/software/" class="list-group-item">Software Saturdays</a>
                 <a href="attendee.php" class="list-group-item">Join as an Attendee</a>
                 <a href="mentor.php" class="list-group-item">Join as a Mentor</a>
-                <a href="android.php" class="list-group-item">Join for Android</a>
             </div>
         </div>
         <!-- Content Column -->
         <div class="col-md-9 text-dark">
             <h2 class="text-center">Join as an Attendee</h2>
             <div class="col-md-12 text-dark">
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdqp00tJjIPkRjLcFvd5Bu34VxZiSwliLMyjhgI-R9zAn1uoQ/viewform?usp=sf_link" width="100%" height="700" frameborder="0">Loading...</iframe>
+                <iframe src="https://goo.gl/forms/F1FxX2QFbRFo3bkv2" width="100%" height="700" frameborder="0">Loading...</iframe>
             </div>
         </div>
     </div>
