470b0cf0c327885263773eb507934b2b78f0e7c4
Ian Sibley <isibley@purdue.edu>
2019-01-08 17:37:54 -0600

    Spring 2019 semester initial update (#165)

--- software/mentor.php
+++ software/mentor.php
@@ -14,17 +14,16 @@
         <!-- Sidebar Column -->
         <div class="col-md-3">
             <div class="list-group">
-                <a href="index.php" class="list-group-item">Software Saturdays</a>
+                <a href="/software/" class="list-group-item">Software Saturdays</a>
                 <a href="attendee.php" class="list-group-item">Join as an Attendee</a>
                 <a href="mentor.php" class="list-group-item">Join as a Mentor</a>
-                <a href="android.php" class="list-group-item">Join for Android</a>
             </div>
         </div>
         <!-- Content Column -->
         <div class="col-md-9 text-dark">
             <h2 class="text-center">Join as a Mentor</h2>
             <div class="col-md-12 text-dark">
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdIuq0jDKHSXarWcxJ9siWDb_Tyn63yew4spm8HI5FG_BHhow/viewform?usp=sf_link" width="100%" height="700" frameborder="0">Loading...</iframe>
+                <iframe src="https://goo.gl/forms/qCTXPvtEuFF4PC4H3" width="100%" height="700" frameborder="0">Loading...</iframe>
             </div>
         </div>
     </div>
