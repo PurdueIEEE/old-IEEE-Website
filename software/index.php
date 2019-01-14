4b84a07adfaac9ffd6f1fd042dc9114061a3a7c4
isibley <isibley@purdue.edu>
2019-01-14 17:46:44 -0500

    Sessions finalized update

--- software/index.php
+++ software/index.php
@@ -36,13 +36,13 @@
 
             <div class="border border-dark">
                 <h3 class="text-center text-primary bg-info">Info Session will be in <b>ARMS B061</b> at <b>3:30 pm</b> on January 19th!</h3>
-                <h4 class="text-center bg-success">Learning Sessions are pending approval for <b>ARMS B061</b> at <b>3:30 pm</b> on Saturdays. Finalized details are aimed to be in place by the end of our first week back.</h4>
-                <h5 class="text-center bg-info">Please see our calendar in the Calendar tab above for more details and sessions as they're finalized</h5>
+                <h4 class="text-center bg-success">Learning Sessions are in <b>ARMS B061</b> at <b>3:30 pm</b> on Saturdays, starting January 26th and continuing weekly.</h4>
+                <h5 class="text-center bg-info">Please see our calendar in the Calendar tab above for more details on sessions, review hours, and project days</h5>
 
                 <p class="text-justify">Software Saturdays aims to foster the learning of coding and languages, and give the Attendees a more rounded learning experience.</p>
                 <p class="text-justify">The plan is to introduce projects and concepts that are useful in the world of Software Development. Regardless of what major our Attendees are pursuing, we aim to teach skills that will be usefull no matter what job or hobbies they pursue in their future.</p>
-                <p class="text-justify">This semester's plan is to do an Android Application Development track showing the basics of Android Studio and healthy application design practices, both in a functional application and in a game aspect.</p>
-                <p class="text-justify">The track was previously comprised of 5 semi bi-weekly sessions, 2-3 hours long with refreshments and breaks included, as well as review hours and two project days. Whether this system will be kept, or a weekly alternative will be adapted for the Android curriculum will hopefully be decided by the semester's second week.</p>
+                <p class="text-justify">This semester we're teaching an Android Application Development track, showing the basics of Android Studio and healthy application design practices, both in a functional application and in a game aspect.</p>
+                <p class="text-justify">The track will be comprised of weekly Saturday sessions around 1.5 hours long with refreshments included, as well as review hours and two to four ending project days that attendees can attend to work during or for help.</p>
                 <p class="text-center">For further questions, feel free to email <b>software-saturdays@purdueieee.org</b> for more information.</p>
             </div>
 
