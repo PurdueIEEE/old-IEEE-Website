b13e7b08ec05a2707932fa41c5073fe876fa84ab
Erik VanderWerf <vanderwe@purdue.edu>
2019-02-26 23:39:24 -0500

    Updated social page.

--- IR/index.php
+++ IR/index.php
@@ -6,25 +6,22 @@
 ?>
 <style>
     .img-lectures {
-        
         border-radius: 5%;
     }
 </style>
 
 <div class="well card-1">
-
         <div class="row">
-
             <div class="col-md-8 text-dark col-md-offset-2 text-dark">
-
                 <h1>IEEE Industrial Relations Committee</h1>
-                <p>The Industrial Relations Committee is in charge of organizing and planning Purdue IEEE’s corporate outreach and professional development programs. Events run by the Industrial Relations Committee include company workshops, training sessions, and recruiting events. The Industrial Relations Committee is also responsible for general Purdue IEEE fundraising activities, as well as assisting technical committee fundraising (from first contact to sponsor follow-up).
+                <p>The Industrial Relations Committee is in charge of organizing and planning Purdue IEEE’s corporate
+                    outreach and professional development programs. Events run by the Industrial Relations Committee
+                    include company workshops, training sessions, and recruiting events. The Industrial Relations
+                    Committee is also responsible for general Purdue IEEE fundraising activities, as well as assisting
+                    technical committee fundraising (from first contact to sponsor follow-up).
                 </p>
-
             </div>
-
         </div>
-
     </div>
     <!-- <div class="well card-1">
     
@@ -34,111 +31,92 @@
     
     </div> -->
     <div class="well card-1">
-
         <div class="row">
-            <img src = "images/microsoft_flyer.jpg" 
-            style = "display: block; margin-left: auto; margin-right: auto; width: 50%;"/>
-        </div>
-
-    </div>
-    <div class="well card-1">
-        <div class="row">
-                <h1 style="color: rgb(29,62,150); text-align: center; font-size: 28px;">
-                Hosted by Purdue IEEE</h1>
-    
-                <h2 style="color: rgb(29,62,100); text-align: center; font-size: 24px;">
-                In Partnership with Sigma Chi, PESC, and Sigma Delta Pi</h2>
-
-                    <div class="col-md-8 text-dark col-md-offset-2 text-dark">
-                    
-                        <h3 style="color: rgb(0,0,0);"> Find Your Job Role </h3>
-                        <p>Learn from industry leaders what it takes to be successful 
-                            in their job roles in a variety of different industries. 
-                            This session will help you orient yourself to be a successful candidate 
-                            when applying for internship and full-time opportunities. <i>Dress code is formal, 
-                            there will be professioal headshots taken.</i></p>
-                        <br>                    
-                        <table class="table table-condensed">
-                            <tr class="active">
-                                <td class="active">Industry</td>
-                                <td class="active">Speaker</td>
-                                <td class="active">Date</td>
-                                <td class="active">Time</td>
-                                <td class="active">Location</td>
-                            </tr>
-                            <tr class="warning">
-                                <td class="success">Academic Technology - <b> Purdue </b></td>
-                                <td class="success">Mitch Daniels</td>
-                                <td class="success">POSTPONED</td>
-                                <td class="success"></td>
-                                <td class="success"></td>
-                            </tr>
-                            <tr class="success">
-                                <td class="success">Clinical Engineering Industry - <b> Franciscan Alliance </b></td>
-                                <td class="success">John Zurisk</td>
-                                <td class="success">4/02/18</td>
-                                <td class="success">6-7 PM</td>
-                                <td class="success">ARMS 1010</td>
-                            </tr>
-                            <tr class="success">
-                                <td class="success">Pharmaceutical Industry - <b>Ultragenyx</b></td>
-                                <td class="success">Shalini Sharp</td>
-                                <td class="success">4/16/18</td>
-                                <td class="success">6-7 PM</td>
-                                <td class="success">WALC 1087</td>
-                            </tr>
-                        </table>
-                        <a href="https://calendar.google.com/event?action=TEMPLATE&tmeid=MjZ0N21rNXFpNHRrN25qZzVjZWRiNHA0dmcgZDVsbzFvanVjdm1lbHNrOWE1NGtuZHExZzhAZw&tmsrc=d5lo1ojucvmelsk9a54kndq1g8%40group.calendar.google.com" target="_blank" style="text-decoration: none;"><img src="images/logo_lockup_calendar_icon.png"></a>
-                    </div>
-        </div>
-    </div>
-
-    <div class="well card-1">
-
-        <div class="row">
-
             <div class="col-md-8 text-dark col-md-offset-2 text-dark">
-                
-                <h2>Previous Events</h2>
-                <hr><br>
+                <h2>Upcoming Events</h2>
+            </div>
+        </div>
+    </div>
 
+    <div class="well card-1">
+        <div class="row">
+            <div class="col-md-8 text-dark col-md-offset-2 text-dark">
+                <h2>Previous Events</h2>
+            </div>
+        </div>
+        <div class="row">
+            <img src = "images/microsoft_flyer.jpg"
+                 style = "display: block; margin-left: auto; margin-right: auto; width: 50%;"/>
+        </div>
+        <br/>
+        <div class="row">
+            <h1 style="color: rgb(29,62,150); text-align: center; font-size: 28px;">
+                Hosted by Purdue IEEE</h1>
+            <h2 style="color: rgb(29,62,100); text-align: center; font-size: 24px;">
+                In Partnership with Sigma Chi, PESC, and Sigma Delta Pi</h2>
+            <div class="col-md-8 text-dark col-md-offset-2 text-dark">
+                <h3 style="color: rgb(0,0,0);"> Find Your Job Role </h3>
+                <p>Learn from industry leaders what it takes to be successful
+                    in their job roles in a variety of different industries.
+                    This session will help you orient yourself to be a successful candidate
+                    when applying for internship and full-time opportunities. <i>Dress code is formal,
+                        there will be professioal headshots taken.</i></p>
+                <br>
+                <table class="table table-condensed">
+                    <tr class="active">
+                        <td class="active">Industry</td>
+                        <td class="active">Speaker</td>
+                        <td class="active">Date</td>
+                        <td class="active">Time</td>
+                        <td class="active">Location</td>
+                    </tr>
+                    <tr class="warning">
+                        <td class="success">Academic Technology - <b> Purdue </b></td>
+                        <td class="success">Mitch Daniels</td>
+                        <td class="success">POSTPONED</td>
+                        <td class="success"></td>
+                        <td class="success"></td>
+                    </tr>
+                    <tr class="success">
+                        <td class="success">Clinical Engineering Industry - <b> Franciscan Alliance </b></td>
+                        <td class="success">John Zurisk</td>
+                        <td class="success">4/02/18</td>
+                        <td class="success">6-7 PM</td>
+                        <td class="success">ARMS 1010</td>
+                    </tr>
+                    <tr class="success">
+                        <td class="success">Pharmaceutical Industry - <b>Ultragenyx</b></td>
+                        <td class="success">Shalini Sharp</td>
+                        <td class="success">4/16/18</td>
+                        <td class="success">6-7 PM</td>
+                        <td class="success">WALC 1087</td>
+                    </tr>
+                </table>
+                <a href="https://calendar.google.com/event?action=TEMPLATE&tmeid=MjZ0N21rNXFpNHRrN25qZzVjZWRiNHA0dmcgZDVsbzFvanVjdm1lbHNrOWE1NGtuZHExZzhAZw&tmsrc=d5lo1ojucvmelsk9a54kndq1g8%40group.calendar.google.com" target="_blank" style="text-decoration: none;"><img src="images/logo_lockup_calendar_icon.png"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 text-dark col-md-offset-2 text-dark">
                 <h3>LEED GREEN Workshop</h3>
                 <img src="images/LGW.png" alt="LEED Green Workshop" height="405" width="720">
-
             </div>
-
         </div>
-
         <br>
-
         <div class="row">
-
             <div class="col-md-8 text-dark col-md-offset-2 text-dark">
-
                 <h3>Cypress PSoC-BLE Tech Talk</h3>
                 <img src="images/PSoC.png" alt="Cypress PSoC-BLE tech talk" height="405" width="720">
-
             </div>
-
         </div>
-
         <br>
-
         <div class="row">
-
             <div class="col-md-8 text-dark col-md-offset-2 text-dark">
-
                 <h3>Industrial-Roundtable Prep Night</h3>
                 <img src="images/IRPrep.png" alt="Cypress PSoC-BLE tech talk" height="540" width="720">
-
             </div>
-
         </div>
-
         <br><br><br>
-
     </div>
-    
 </div>
 
 <?php include '../footer.php'; ?>
