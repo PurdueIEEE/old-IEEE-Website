5420ad1a61b88fe47da4d23717225c90919b228b
Swagat <swagat.bhattacharyya@gmail.com>
2020-01-19 12:15:45 -0500

    Big C-Society page changes -> More like ROV page now

--- csociety/index.php
+++ csociety/index.php
@@ -1,49 +1,78 @@
 <?php
-    $site_title = 'Computer Society';
-    $site_index = '/csociety';
-    include '../header.php';
+$site_title = 'Computer Society';
+$site_index = '/csociety';
+include '../header.php';
 ?>
-    <link href="/assets/bootstrap.csociety.css" rel="stylesheet">
 
-    <!-- Content Row -->
-    <div class="well card-1">
-        <div class="row">
+<link href="/assets/bootstrap.csociety.css" rel="stylesheet">
 
-            <!-- Content Column -->
-            <div class="col-md-8 text-dark col-md-offset-2 text-dark">
-                <h2>Computer Society</h2>
-                <p>We are a group of highly curious people with quirks in the science of computation. We do cool stuff with computers and try to spread the enlightenment with who-so-ever matches the level of curiosity of the group. 3D printing, web developing, cyber security, high performance computing, machine learning, graphics and everything you've heard of as tech-y is discussed, talked and made here, feel free to drop by. Contact Erik Wilson at <a href="mailto:wilso969@purdue.edu">wilso969@purdue.edu</a> for more details.</p>
-                <a href="https://twitter.com/PurdueCSociety"><i class="fa fa-twitter-square fa-4x"></i></a>
-                <a href="/csociety/images/csociety.jpg"
-                    <p class="text-center"><img src="/csociety/images/csociety.jpg" alt="2016 Team" style="width:100%"></p>
-                </a>
-            </div>
-        </div>
-    </div>
+<!-- Content Row -->
+<div class="well card-1">
+    <div class="row">
 
-    <div class="well card-1 text-dark">
-        <div class="row text-center">
-
-            <h1 id="leaders">Leadership</h1>
-            <hr/>
-            <div class="row">
-                <div class="col-md-4">
-                    <h4>Erik Wilson</h4>
-                    <p>Chair<br/><br/></p>
-                </div>
-                <div class="col-md-4">
-                    <h4>Blaise Sellers</h4>
-                    <p>Vice Chair<br/><br/></p>
-                </div>
-                <div class="col-md-4">
-                    <h4>Jerome Schweitzer</h4>
-                    <p>Event Coordinator<br/> <br/></p>
-                </div>
-
+        <!-- Sidebar Column -->
+        <div class="col-md-3">
+            <div class="list-group">
+                <a href="/csociety/projects/" class="list-group-item">Projects</a>
+                <a href="/csociety/Bylaws.pdf" class="list-group-item">Bylaws</a>
             </div>
         </div>
 
-    </div>
-    <!-- /.well -->
+        <!-- Content Column -->
+        <div class="col-md-7 text-dark col-md-offset-1">
+            <h2 class="text-center">IEEE Computer Society</h2>
+            <br>
+            <hr>
 
-<?php include '../footer.php';?>
+            <h2 class="text-center">About C-Society</h2>
+            <div class="col-md-6">
+                <img src="/csociety/images/csociety.jpg" alt="2016 Team" style="width:100%"
+                     style="float:left" style="display:block">
+                <p class="text-center">2016 Purdue Supercomputer Tour</p>
+            </div>
+
+            <p class="text-justify">
+                We are a group of highly curious people with quirks in the science of computation. We do cool stuff with computers and try to spread the enlightenment with who-so-ever matches the level of curiosity of the group. 3D printing, web developing, cyber security, high performance computing, machine learning, graphics and everything you've heard of as tech-y is discussed, talked and made here, feel free to drop by. Contact Erik Wilson at <a href="mailto:wilso969@purdue.edu">wilso969@purdue.edu</a> for more details.
+            </p>
+			<br>
+
+            <h2 class="text-center">2019-2020 C-Society Leadership</h2>
+            <a href="/csociety/images/2019_Leadership.jpg"
+            <p class="text-center"><img src="/csociety/images/2019_Leadership.jpg" alt="2019 Leaders" style="width:100%"></p>
+            </a>
+
+			<div class="row text-center">
+				<div class="row">
+					<div class="col-md-1">
+					</div>
+					<div class="col-md-2">
+						<h4>Jerome S.</h4>
+						<p>Event Coordinator<br/><br/></p>
+					</div>
+					<div class="col-md-2">
+						<h4>Alex F.</h4>
+						<p>Sponsorship Coordinator<br/><br/></p>
+					</div>
+					<div class="col-md-2">
+						<h4>Erik W.</h4>
+						<p>Chair<br/> <br/></p>
+					</div>
+					<div class="col-md-2">
+						<h4>Ritvik K.</h4>
+						<p>Vice-Chair<br/> <br/></p>
+					</div>
+					<div class="col-md-2">
+						<h4>Justin C.</h4>
+						<p>Workshop Coordinator<br/> <br/></p>
+					</div>
+				</div>
+			</div>
+
+            <p class="text-center"><a href="https://twitter.com/PurdueCSociety"><i class="fa fa-twitter-square fa-4x"></i></a></p>
+            <br>
+        </div>
+    </div>
+</div>
+<!-- /.well -->
+
+<?php include '../footer.php'; ?>
---
+++ csociety/projects/index.php
@@ -0,0 +1,31 @@
+<?php
+    $page_title = 'Projects';
+    $site_title = 'Computer Society';
+    $site_index = '/csociety';
+    include '../../header.php';
+?>
+
+<link href="/assets/bootstrap.csociety.css" rel="stylesheet">
+
+<!-- Content Row -->
+<div class="well card-1">
+    <div class="row">
+
+        <!-- Sidebar Column -->
+        <div class="col-md-3">
+            <div class="list-group">
+                <a href="/csociety/projects/" class="list-group-item">Projects</a>
+                <a href="/csociety/Bylaws.pdf" class="list-group-item">Bylaws</a>
+            </div>
+        </div>
+
+        <!-- Content Column -->
+        <div class="col-md-7 text-dark col-md-offset-1">
+        <h1 class="text-center">Coming Soon!</h1>
+            <p>For now, if you are interested in seeing past C-Society projects, please check out our <a href="https://github.com/PurdueIEEEComputerSociety/">GitHub Repositories</a>!</p><br>
+        </div>
+		
+    </div>
+    <!-- /.well -->
+
+    <?php include '../../footer.php';?>
