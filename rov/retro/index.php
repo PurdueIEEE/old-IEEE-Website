24258a8905b4851a4dd518167688a57b7b3f7446
Grant Geyer <GrantG012@yahoo.com>
2020-05-26 20:32:51 -0400

    got improved sidebar from ggeyer-retro

--- rov/retro/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $page_title = 'Retro ROV';
-    $site_title = 'IEEE ROV';
-    $site_index = '/rov';
-?>
-
-<div style="width:100%">
-	<img src="/rov/images/Engineering.png" alt="College of Engineering" style="width:100%; margin-left: 100px; height: 100px;">
-</div>
