1cd0541ed3b5c8f29d5c77b966a097646dfe4255
Grant Geyer <GrantG012@yahoo.com>
2020-09-16 12:23:57 -0400

    updated rov join for closed applications and spring '21 joining

--- rov/join.php
+++ rov/join.php
@@ -22,7 +22,7 @@ include '../header.php';
             <h2 class="text-center">Join ROV</h2>
 
 			<p>
-				The ROV Fall 2020 Info Session will be held at Wednesday, September 9th at 6:30 in ME 1130 (pending approval).
+				The ROV Fall 2020 Info Session was held on Wednesday, September 9th at 6:30 in ME 1130.
 				Though not prefered, the info session can be joined virtually, in the
 				ROV channel of the <a href="https://discordapp.com/invite/fFNnKPK">IEEE Q&A discord</a>
 			</p>
@@ -31,13 +31,14 @@ include '../header.php';
                 The ROV team brings together expertise in electronics, mechanics, and software in order to create an
                 unrivaled vehicle. We strive to finish first place in the world, and we are therefore looking for
                 dedicated and talented members to help us achieve this goal! If you enjoy being a part of a fast-paced
-                team and learn quickly look no further.
+                team and learn quickly, look no further.
             </p>
 
             <p>
-               Applications for the 2020 Fall semester are now open!
-               <u><a href="https://docs.google.com/forms/d/e/1FAIpQLSeth3z0i0iMqrmDyHHm2cs1e6QQc7lPG10GdWTnvpd2SOX43Q/viewform?usp=sf_link">here</a></u>.
+               Applications for the 2020 Fall semester are closed.
+               <!-- <u><a href="https://docs.google.com/forms/d/e/1FAIpQLSeth3z0i0iMqrmDyHHm2cs1e6QQc7lPG10GdWTnvpd2SOX43Q/viewform?usp=sf_link">here</a></u>. -->
                Applications must be filled out by 11:59 pm on Friday, September 11th. If you have any questions please reach out to Grant Geyer at <a href="mailto:rov@purdueieee.org">rov@purdueieee.org</a>.
+			   The team will be accepting new members again at the start of the Spring 2021 semester.
                <hr>
                <!--
                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfH-CqASDCHd7Ufl6dEkFcpMXlWsgE0pazMNHB8NVjM82jhiA/viewform?usp=sf_link"
