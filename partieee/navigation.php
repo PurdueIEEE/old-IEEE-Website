a89c4b6ffd1c3812c41d70080d0c0985a899514e
Hadi Ahmed <hadiahmed098@gmail.com>
2020-11-06 13:07:41 -0500

    Updates to PART and Software Saturdays (#198)
    * Fixed some spelling on PART page
    
    * Updated Software Saturdays and added info
    
    * Fixed typo

--- partieee/navigation.php
+++ partieee/navigation.php
@@ -4,7 +4,7 @@
         <a href="about.php" class="list-group-item part-about-sidelink">About Us</a>
         <a href="part_bylaws.pdf" class="list-group-item">Bylaws</a>
         <a href="http://www.auvsi-suas.org/" target="_blank" class="list-group-item">SUAS Competition</a>
-        <a href="sponsorships.php" class="list-group-item part-sponsership-sidelink">Sponsorship</a>
+        <a href="sponsorships.php" class="list-group-item part-sponsorship-sidelink">Sponsorship</a>
         <a href="media.php" class="list-group-item part-media-sidelink">Media</a>
     </div>
 </div>
