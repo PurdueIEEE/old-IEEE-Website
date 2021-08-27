e4fba1c97085c1c24673537711d2e0ad03635e4c
Hadi Ahmed <hadiahmed098@gmail.com>
2021-08-26 20:06:57 -0400

    Learning info session

--- info_sessions.php
+++ info_sessions.php
@@ -15,6 +15,7 @@ $etime = function ($month, $day, $hour, $minute = 0) use ($year, $tz) {
 $info_sessions = array(
     array($etime(9, 1, 18, 30), $etime(9, 1, 20, 0), 'PHYS 112', GENERAL),
     array($etime(9, 2, 18, 30), $etime(9, 2, 20, 0), 'PHYS 112', GENERAL),
+    array($etime(9, 5, 16, 0), $etime(9, 5, 17, 0), '<a href="https://discord.gg/kZNkKXM">IEEE Q&A Discord [Learning]</a>', LEARNING),
     array($etime(9, 8, 18, 0), $etime(9, 8, 20, 0), 'POTR 234', ROV),
     array($etime(9, 8, 18, 30), $etime(9, 8, 19, 30), 'ARMS 1010', PARTIEEE),
     /*array($etime(9, 9, 0, 0), $etime(9, 9, 0, 1), '', EMBS),*/
@@ -22,7 +23,6 @@ $info_sessions = array(
     array($etime(9, 9, 19, 0), $etime(9, 9, 20, 0), '<a href="https://discord.gg/kZNkKXM">IEEE Q&A Discord [MTT-S]</a>', MTTS),
     array($etime(9, 11, 15, 30), $etime(9, 11, 16, 30), '<a href="https://purdue-edu.zoom.us/j/99675779158?pwd=Vklib29nS1BCYVpXaG5wMkRlUnpTQT09">WALC 1087 and Zoom</a>', SOFTWARE_SATURDAYS),
 	/*array($etime(1, 28, 19), $etime(1, 28, 20), '<a href="https://discord.gg/cVsCYVma">Racing Discord [https://discord.gg/cVsCYVma]</a>', RACING),
-	array($etime(1, 31, 13),     $etime(1, 31, 14), '<a href="https://discord.gg/bh3ENZy">Learning Discord [https://discord.gg/bh3ENZy]</a>', LEARNING),
 	array($etime(1, 31, 16, 30), $etime(1, 31, 17, 30), '<a href="https://discord.gg/kZNkKXM">IEEE Q&A Discord [Social]</a>', SOCIAL),
 	array($etime(2, 1, 18), $etime(2, 1, 19), '<a href="https://discord.gg/kZNkKXM">IEEE Q&A Discord [Industrial Relations]</a>', INDUSTRIAL_RELATIONS),
 	array($etime(2, 5, 18, 30), $etime(2, 5, 19, 30), '<a href="https://discord.gg/kZNkKXM">IEEE Q&A Discord [G&E]</a>', GROWTH)*/
